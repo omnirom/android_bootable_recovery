c3885fabda7f8a4bd39f42f76012e00514b0a3ff
Doug Zongker <dougz@android.com>
2009-06-11 17:05:58 -0700

    fix simulator build by excluding more of recovery

--- Android.mk
+++ Android.mk
@@ -41,12 +41,13 @@ LOCAL_STATIC_LIBRARIES += libstdc++ libc
 
 include $(BUILD_EXECUTABLE)
 
-endif   # TARGET_ARCH == arm
-endif	# !TARGET_SIMULATOR
-
 include $(commands_recovery_local_path)/minui/Android.mk
 include $(commands_recovery_local_path)/amend/Android.mk
 include $(commands_recovery_local_path)/minzip/Android.mk
 include $(commands_recovery_local_path)/mtdutils/Android.mk
 include $(commands_recovery_local_path)/tools/Android.mk
 commands_recovery_local_path :=
+
+endif   # TARGET_ARCH == arm
+endif	# !TARGET_SIMULATOR
+
