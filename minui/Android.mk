4c3c8e5eb4ad8f5bbefec52c6cc7d403e4f6eebf
Koushik Dutta <koushd@gmail.com>
2011-11-16 16:47:53 -0800

    Support custom graphics.c.
    Conflicts:
    	Android.mk
    	ui.c
    
    Change-Id: Ic7d10e8230151bcc4be6cfa222a1e07231e7139f

--- minui/Android.mk
+++ minui/Android.mk
@@ -1,7 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := graphics.c events.c resources.c
+LOCAL_SRC_FILES := events.c resources.c
+ifneq ($(BOARD_CUSTOM_GRAPHICS),)
+  LOCAL_SRC_FILES += $(BOARD_CUSTOM_GRAPHICS)
+else
+  LOCAL_SRC_FILES += graphics.c
+endif
 
 LOCAL_C_INCLUDES +=\
     external/libpng\
