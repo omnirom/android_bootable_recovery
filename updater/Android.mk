af50449efd9ba80c936963c8b53a830f9997e703
Dees_Troy <dees_troy@teamw.in>
2012-10-11 12:00:29 -0400

    Fix updater binary compile

--- updater/Android.mk
+++ updater/Android.mk
@@ -22,8 +22,7 @@ ifeq ($(TARGET_USERIMAGES_USE_EXT4), true)
 LOCAL_CFLAGS += -DUSE_EXT4
 LOCAL_C_INCLUDES += system/extras/ext4_utils
 LOCAL_STATIC_LIBRARIES += \
-    libext4_utils_static \
-    libsparse_static \
+    libext4_utils \
     libz
 endif
 
