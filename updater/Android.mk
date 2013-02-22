43681d4497d2ba28e409263a4c91d8190dfbddcb
Dees_Troy <dees_troy@teamw.in>
2013-02-22 21:26:35 +0000

    Fix building of updater for 4.2 environment
    Change-Id: I2dc6690f77f4fbaca0ab0ff849b8b07875129827

--- updater/Android.mk
+++ updater/Android.mk
@@ -24,6 +24,12 @@ LOCAL_C_INCLUDES += system/extras/ext4_utils
 LOCAL_STATIC_LIBRARIES += \
     libext4_utils \
     libz
+ifneq ($(wildcard system/core/libmincrypt/rsa_e_3.c),)
+LOCAL_STATIC_LIBRARIES = \
+    libext4_utils_static \
+    libsparse_static \
+    libz
+endif
 endif
 
 ifeq ($(HAVE_SELINUX), true)
