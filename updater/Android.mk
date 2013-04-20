76a1d414299f6a1dd41e6c82af4f756c15f0e313
Dees_Troy <dees_troy@teamw.in>
2013-04-20 13:54:35 +0000

    Fix write_raw_image to work with mmc

--- updater/Android.mk
+++ updater/Android.mk
@@ -40,6 +40,7 @@ endif # HAVE_SELINUX
 
 LOCAL_STATIC_LIBRARIES += $(TARGET_RECOVERY_UPDATER_LIBS) $(TARGET_RECOVERY_UPDATER_EXTRA_LIBS)
 LOCAL_STATIC_LIBRARIES += libapplypatch libedify libmtdutils libminzip libz
+LOCAL_STATIC_LIBRARIES += libflashutils libmmcutils libbmlutils
 LOCAL_STATIC_LIBRARIES += libmincrypt libbz
 LOCAL_STATIC_LIBRARIES += libminelf
 LOCAL_STATIC_LIBRARIES += libcutils libstdc++ libc
