71e9b049c21fb01d558f49844d17e792c347a930
Samer Diab (S.a.M.e.R_d) <remasdiab@gmail.com>
2014-01-07 20:18:47 +0000

    Add haptic feedback
    Change-Id: I15d723f73c0b0bb4a40645d1c3f4c1c616f36998

--- data.hpp
+++ data.hpp
@@ -55,6 +55,7 @@ public:
 
 	static void DumpValues();
 	static void update_tz_environment_variables();
+	static void Vibrate(const string varName);
 	static void SetBackupFolder();
 	static void SetDefaultValues();
 	static void Output_Version(void); // Outputs the version to a file in the TWRP folder
