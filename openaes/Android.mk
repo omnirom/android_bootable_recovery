e298493f98b15d9c7c9dd916437c476c3ca0ee57
Ethan Yonker <dees_troy@teamw.in>
2014-02-11 11:43:29 -0600

    Add rule to make libopenaes_static
    Change-Id: Id4b6d5d9364a3c3a3c2a367f961fcc7f7a1107e7

--- openaes/Android.mk
+++ openaes/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 ifneq ($(TW_EXCLUDE_ENCRYPTED_BACKUPS), true)
+	# Build shared binary
 	LOCAL_SRC_FILES:= src/oaes.c \
 	LOCAL_C_INCLUDES := \
 		bootable/recovery/openaes/src/isaac \
@@ -14,6 +15,7 @@ ifneq ($(TW_EXCLUDE_ENCRYPTED_BACKUPS), true)
 	LOCAL_SHARED_LIBRARIES = libopenaes libc
 	include $(BUILD_EXECUTABLE)
 
+	# Build shared library
 	include $(CLEAR_VARS)
 	LOCAL_MODULE := libopenaes
 	LOCAL_MODULE_TAGS := eng
@@ -23,4 +25,15 @@ ifneq ($(TW_EXCLUDE_ENCRYPTED_BACKUPS), true)
 	LOCAL_SRC_FILES = src/oaes_lib.c src/isaac/rand.c
 	LOCAL_SHARED_LIBRARIES = libc
 	include $(BUILD_SHARED_LIBRARY)
+
+	# Build static library
+	include $(CLEAR_VARS)
+	LOCAL_MODULE := libopenaes_static
+	LOCAL_MODULE_TAGS := eng
+	LOCAL_C_INCLUDES := \
+		bootable/recovery/openaes/src/isaac \
+		bootable/recovery/openaes/inc
+	LOCAL_SRC_FILES = src/oaes_lib.c src/isaac/rand.c
+	LOCAL_STATIC_LIBRARIES = libc
+	include $(BUILD_STATIC_LIBRARY)
 endif
