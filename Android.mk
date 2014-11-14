9d52cf2efb5a0b07d40f552ad9127c4f3fedfaa2
Matt Mower <mowerm@gmail.com>
2014-11-14 10:54:03 -0600

    Fix mkexfatfs exclusion when TW_NO_EXFAT enabled
    Push to android-5.0
    
    Conflicts:
    	Android.mk
    
    Change-Id: I0b831a4e6e52e4b314751035b750e6a398cb4d2d

--- Android.mk
+++ Android.mk
@@ -334,7 +334,6 @@ LOCAL_ADDITIONAL_DEPENDENCIES := \
     flash_image \
     fix_permissions.sh \
     mke2fs.conf \
-    mkexfatfs \
     pigz \
     teamwin \
     toolbox_symlinks \
@@ -351,6 +350,9 @@ ifneq ($(TARGET_ARCH), arm64)
 endif
 ifneq ($(TW_USE_TOOLBOX), true)
     LOCAL_ADDITIONAL_DEPENDENCIES += busybox_symlinks
+endif
+ifneq ($(TW_NO_EXFAT), true)
+    LOCAL_ADDITIONAL_DEPENDENCIES += mkexfatfs
 endif
 ifeq ($(BOARD_HAS_NO_REAL_SDCARD),)
     LOCAL_ADDITIONAL_DEPENDENCIES += parted
