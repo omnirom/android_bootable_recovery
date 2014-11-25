f15976d07d903d9c4ddd501254e1533d62b88794
Ethan Yonker <dees_troy@teamw.in>
2014-11-25 15:00:52 -0600

    Reduce libs needed for decrypt and clean up old decypt files
    Trim cryptfs.c to remove functions that TWRP does not use for
    decrypt and remove the need for libfs_mgr from cryptfs.c by
    passing some items to cryptfs.c from the partition manager.
    
    Clean up decrypt and only support one version
    
    Android 5.0 lollipop decrypt should be backwards compatible with
    older versions so we will only support one version, 1.3 that came
    with 5.0 lollipop.
    
    Remove support for Samsung TouchWiz decrypt. It does not work with
    the latest versions of Samsung encryption anyway and it has not
    been updated to work with any AOSP decryption higher than 1.1
    
    Change-Id: I2d9c6e31df50268c91ee642c2fa090f901d9d5c9

--- crypto/scrypt/Scrypt-config.mk
+++ crypto/scrypt/Scrypt-config.mk
@@ -77,7 +77,7 @@ target_arch := unknown_arch
 endif
 
 target_c_flags    := $(common_c_flags) $($(target_arch)_c_flags) $(local_c_flags)
-target_c_includes := $(addprefix $(commands_recovery_local_path)/crypto/scrypt/,$(common_c_includes)) $(local_c_includes)
+target_c_includes := $(addprefix external/scrypt/,$(common_c_includes)) $(local_c_includes)
 target_src_files  := $(common_src_files) $($(target_arch)_src_files)
 target_src_files  := $(filter-out $($(target_arch)_exclude_files), $(target_src_files))
 
@@ -97,7 +97,7 @@ host_arch := unknown_arch
 endif
 
 host_c_flags    := $(common_c_flags) $($(host_arch)_c_flags) $(local_c_flags)
-host_c_includes := $(addprefix $(commands_recovery_local_path)/crypto/scrypt/,$(common_c_includes)) $(local_c_includes)
+host_c_includes := $(addprefix external/scrypt/,$(common_c_includes)) $(local_c_includes)
 host_src_files  := $(common_src_files) $($(host_arch)_src_files)
 host_src_files  := $(filter-out $($(host_arch)_exclude_files), $(host_src_files))
 
