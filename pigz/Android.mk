b47f28b05ad67417aaf94969726af5005e10c99b
Dees Troy <dees_troy@teamw.in>
2014-12-11 17:20:59 +0000

    Fix generating symlinks for older trees
    Symlinks for unpigz, toolbox, and busybox were not generating in
    older trees.
    
    Change-Id: I3ca2a2047510954e66fb3cf49eef6af713d3268e

--- pigz/Android.mk
+++ pigz/Android.mk
@@ -23,6 +23,15 @@ $(SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@
 	$(hide) ln -sf $(PIGZ_BINARY) $@
 
+ifneq (,$(filter $(PLATFORM_SDK_VERSION),16 17 18))
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
+
+# We need this so that the installed files could be picked up based on the
+# local module name
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
+	$(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := unpigz_symlink
 LOCAL_MODULE_TAGS := optional
