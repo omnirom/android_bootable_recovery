5ed58ccf31de14dcec1034cd96b8549d32eb00fb
Kra1o5 <kra1o5x@gmail.com>
2014-12-17 01:49:00 +0100

    Add support for custom kernel headers
    Change-Id: I39fe4de8919e59dd75e8cb535fa0648948035c26

--- minuitwrp/Android.mk
+++ minuitwrp/Android.mk
@@ -16,7 +16,11 @@ ifeq ($(TW_TARGET_USES_QCOM_BSP), true)
     LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
   else
-    LOCAL_C_INCLUDES += $(commands_recovery_local_path)/minuitwrp/include
+    ifeq ($(TARGET_CUSTOM_KERNEL_HEADERS),)
+      LOCAL_C_INCLUDES += $(commands_recovery_local_path)/minuitwrp/include
+    else
+      LOCAL_C_INCLUDES += $(TARGET_CUSTOM_KERNEL_HEADERS)
+    endif
   endif
 else
   LOCAL_C_INCLUDES += $(commands_recovery_local_path)/minuitwrp/include
