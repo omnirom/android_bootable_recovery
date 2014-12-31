183a6f88d05097428c81bca571a679f2ec9292c3
that <github@that.at>
2014-12-31 18:30:50 +0100

    crypto: remove unused libs and clean up makefile
    libsoftkeymaster and its dependencies appear to be unused.
    
    Change-Id: Ib720f5e4d2750a739ba6b65b346c0e167df279d3

--- crypto/lollipop/Android.mk
+++ crypto/lollipop/Android.mk
@@ -2,51 +2,12 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TW_INCLUDE_CRYPTO), true)
 include $(CLEAR_VARS)
 
-common_c_includes := \
-	system/extras/ext4_utils \
-	system/extras/f2fs_utils \
-	external/openssl/include \
-	external/stlport/stlport \
-	bionic \
-	$(commands_recovery_local_path)/crypto/scrypt/lib/crypto \
-	frameworks/native/include \
-	system/security/keystore \
-	hardware/libhardware/include/hardware \
-	system/security/softkeymaster/include/keymaster
-
-common_shared_libraries := \
-	libsysutils \
-	libstlport \
-	libbinder \
-	libcutils \
-	liblog \
-	libdiskconfig \
-	liblogwrap \
-	libext4_utils \
-	libf2fs_sparseblock \
-	libcrypto \
-	libselinux \
-	libutils \
-	libminhardware \
-	libsoftkeymaster
-
-common_shared_libraries := libcrypto libselinux libhardware libsoftkeymaster libcutils
-
 LOCAL_MODULE := libcryptfslollipop
 LOCAL_MODULE_TAGS := eng optional
 LOCAL_CFLAGS :=
 LOCAL_SRC_FILES = cryptfs.c
-#LOCAL_C_INCLUDES += \
-#    system/extras/ext4_utils \
-#    external/openssl/include \
-#    system/extras/f2fs_utils \
-#    external/scrypt/lib/crypto \
-#    system/security/keystore \
-#    hardware/libhardware/include/hardware \
-#	system/security/softkeymaster/include/keymaster
-#LOCAL_SHARED_LIBRARIES += libc liblog libcutils libcrypto libext4_utils
-LOCAL_SHARED_LIBRARIES := $(common_shared_libraries) libmincrypttwrp
-LOCAL_C_INCLUDES := external/openssl/include $(common_c_includes)
+LOCAL_SHARED_LIBRARIES := libcrypto libhardware libcutils
+LOCAL_C_INCLUDES := external/openssl/include $(commands_recovery_local_path)/crypto/scrypt/lib/crypto
 LOCAL_WHOLE_STATIC_LIBRARIES += libscrypttwrp_static
 
 include $(BUILD_SHARED_LIBRARY)
