07d9627d7cf31e03b778bc4b1c918d552680cd28
Ajay Dudani <adudani@codeaurora.org>
2015-02-04 16:51:34 -0800

    recovery: Handle EV_SW events
    Change-Id: I29c13a941067cd1bbdced3bd67fd93293347d7b6

--- minui/events.c
+++ minui/events.c
@@ -80,8 +80,8 @@ int ev_init(ev_callback input_cb, void *data)
             }
 
             /* TODO: add ability to specify event masks. For now, just assume
-             * that only EV_KEY and EV_REL event types are ever needed. */
-            if (!test_bit(EV_KEY, ev_bits) && !test_bit(EV_REL, ev_bits)) {
+             * that only EV_KEY, EV_REL & EV_SW event types are ever needed. */
+            if (!test_bit(EV_KEY, ev_bits) && !test_bit(EV_REL, ev_bits) && !test_bit(EV_SW, ev_bits)) {
                 close(fd);
                 continue;
             }
