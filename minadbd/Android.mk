1ddd35050417b79307c5e0a989ee39d6ce2bfc1b
Dan Albert <danalbert@google.com>
2015-02-18 15:58:15 -0800

    Use headers from adb.
    adb.h has diverged a bit, so that one will be more involved, but these
    three are all trivial, unimportant changes.
    
    Change-Id: Ief8474c1c2927d7e955adf04f887c76ab37077a6

--- minadbd/Android.mk
+++ minadbd/Android.mk
@@ -18,7 +18,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_CFLAGS := -O2 -g -DADB_HOST=0 -Wall -Wno-unused-parameter
 LOCAL_CFLAGS += -D_XOPEN_SOURCE -D_GNU_SOURCE
-LOCAL_C_INCLUDES += bootable/recovery
+LOCAL_C_INCLUDES := bootable/recovery system/core/adb
 
 LOCAL_MODULE := libminadbd
 
