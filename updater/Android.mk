873e15eeb86ec92232bf1718816d19ed404c56b1
Tom Marshall <tdm@cyngn.com>
2015-04-17 05:23:00 -0700

    recovery: Add lz4 libs if available
     * Required by transparent compresssion in make_ext4fs.
    
    Change-Id: I566c9e1281cc0dd725e96db32da0a1c3c000030e

--- updater/Android.mk
+++ updater/Android.mk
@@ -38,6 +38,9 @@ LOCAL_STATIC_LIBRARIES = \
     libsparse_static \
     libz
 endif
+ifneq ($(wildcard external/lz4/Android.mk),)
+    LOCAL_STATIC_LIBRARIES += liblz4-static
+endif
 endif
 
 LOCAL_STATIC_LIBRARIES += $(TARGET_RECOVERY_UPDATER_LIBS) $(TARGET_RECOVERY_UPDATER_EXTRA_LIBS)
