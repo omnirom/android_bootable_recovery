0ef6492518593eb1255e83723aaf9195fb582d6f
andi34 <skate4life@gmx.de>
2015-05-09 10:48:52 -0700

    updater: Allow devices to suppress BLKDISCARD
    * On some devices TRIM is disabled for security reasons. Don't fail
      flashing the ROM because discard isn't possible in this case.
    
    Change-Id: I044619c3e0b01a496d967ef136501d0190240ad4

--- updater/Android.mk
+++ updater/Android.mk
@@ -74,6 +74,10 @@ LOCAL_CFLAGS := \
     -Wall \
     -Werror
 
+ifeq ($(BOARD_SUPPRESS_EMMC_WIPE),true)
+    LOCAL_CFLAGS += -DSUPPRESS_EMMC_WIPE
+endif
+
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
     $(LOCAL_PATH)/include
 
