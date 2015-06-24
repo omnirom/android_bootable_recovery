ba9a42aa7e10686de186636fe9fecbf8c4cc7c19
Tao Bao <tbao@google.com>
2015-06-23 23:23:33 -0700

    recovery: Switch applypatch/ and updater/ to cpp.
    Mostly trivial changes to make cpp compiler happy.
    
    Change-Id: I69bd1d96fcccf506007f6144faf37e11cfba1270

--- updater/Android.mk
+++ updater/Android.mk
@@ -1,11 +1,23 @@
 # Copyright 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
 
 updater_src_files := \
-	install.c \
-	blockimg.c \
-	updater.c
+	install.cpp \
+	blockimg.cpp \
+	updater.cpp
 
 #
 # Build a statically-linked binary to include in OTA packages
