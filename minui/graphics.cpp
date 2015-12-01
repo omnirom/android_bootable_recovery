39624d4e5e724e0de68cecab0594df30820d79a2
maxwen <max.weninger@gmail.com>
2015-12-01 22:02:07 +0100

    minui: bring back custom font support
    Change-Id: I6cb97b236ff8ada46d270e99ab37656f5f1b73f8

--- minui/graphics.cpp
+++ minui/graphics.cpp
@@ -31,7 +31,12 @@
 
 #include <time.h>
 
+#ifdef BOARD_USE_CUSTOM_RECOVERY_FONT
+#include BOARD_USE_CUSTOM_RECOVERY_FONT
+#else
 #include "font_10x18.h"
+#endif
+
 #include "minui.h"
 #include "graphics.h"
 
