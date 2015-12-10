065c82b0ae7de017446a30068a7b13a00bf41921
Ethan Yonker <dees_troy@teamw.in>
2015-12-10 10:19:45 -0600

    Adopted Storage support
     -Detects, decrypts, and mounts an adopted SD card if a
      secondary block device is defined (usually mmcblk1)
     -Handles unified storage
     -Displays the adopted storage in MTP along with internal
     -Factory Reset - wiped just like a data media device, we
      retain the keys folder and the storage.xml during a
      factory reset
     -Backup / Restore
     -Disable mass storage when adopted storage is present
     -Read storage nickname from storage.xml and apply it to
      display names in the GUI
     -Read storage.xml and determine what storage location is in
      use for /sdcard and remap accordingly
    
    libgpt_twrp is source code mostly kanged from an efimanager
    project. It is GPL v2 or higher, so we will opt for GPL v3.
    
    Change-Id: Ieda0030bec5155ba8d2b9167dc0016cebbf39d55

--- gui/action.cpp
+++ gui/action.cpp
@@ -1467,6 +1467,7 @@ int GUIAction::decrypt(std::string arg __unused)
 					LOGINFO("Got default contexts and file mode for storage files.\n");
 				}
 			}
+			PartitionManager.Decrypt_Adopted();
 		}
 	}
 
