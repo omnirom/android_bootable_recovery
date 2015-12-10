65cedf12409d6f3e9a2a4d0b8a583dd180abc6e5
Ethan Yonker <dees_troy@teamw.in>
2015-12-10 10:19:45 -0600

    WIP: adopted storage support
    Working:
     -Detects, decrypts, and mounts an adopted SD card if a
      secondary block device is defined (usually mmcblk1)
     -Handles unified storage
     -Displays the adopted storage in MTP along with internal
     -Factory Reset
     -Backup / Restore (probably)
     -Disable mass storage when adopted storage is present
     -Read storage nickname from storage.xml and apply it to
      display names in the GUI
     -Read storage.xml and determine what storage location is in
      use for /sdcard and remap accordingly
    
    There are a number of issues that will need to be decided on:
     -Do we want to change the display name? -- read from xml
     -How do we handle factory resets? -- the adopted storage will
      be wiped during a regular factory reset.
     -Can we factory reset and leave the expand_* key in place and
      still have the adopted storage work? -- YES
     -How do we want to allow for wiping? If we can leave the
      adopted storage in place, do we allow for un-adopting?
     -Do we want to determine which device is the /sdcard device?
      -- YES, scanning the storage.xml file and matching up
     -Are we handling settings storage properly? -- should still
      be using internal storage for settings storage
     -If we un-adopt, we will have to change the partition table
     -Should the adopted storage be automatically included in a
      regular Data backup or should it be a separate item? -- YES
     -Handle multiple adopted storage devices? -- anything that
      is identified as removable storage should be able to be
      handled as an adopted storage device in TWRP
     -fstab flags? -- probably don't need any
    
    libgpt_twrp is source code mostly kanged from an efimanager
    project. It is GPL v2 or higher, so we will opt for GPL v3.
    
    Change-Id: Ieda0030bec5155ba8d2b9167dc0016cebbf39d55

--- gui/action.cpp
+++ gui/action.cpp
@@ -1457,6 +1457,7 @@ int GUIAction::decrypt(std::string arg __unused)
 					LOGINFO("Got default contexts and file mode for storage files.\n");
 				}
 			}
+			PartitionManager.Decrypt_Adopted();
 		}
 	}
 
