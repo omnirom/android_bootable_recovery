4c538b8efc5118465df5461a39e968c7fa1b6369
bigbiff <bigbiff@teamw.in>
2015-12-12 18:30:21 -0500

    ADB: Add adb backup for TWRP.
    Functionality for client side to backup
    tar and image streams over adbd to the client under backup.ab.
    
    Requires: https://gerrit.omnirom.org/#/c/14677/ cherry-pick.
    Using adb backup on the client side you can backup the partitions
    TWRP knows about.
    
    On the client side you can do the following:
    adb backup <options> where options are
    --compress: compress data
    --system: backup system
    --cache: backup cache
    --data: backup data
    --boot: backup boot
    etc...
    
    This will backup your data to a filename backup.ab on the client.
    You can then rename the file and encrypt it with desktop tools.
    To restore the backup use the following convention:
    adb restore <filename>
    
    We are using structures to store metadata as binary inside
    of the file itself. If the metadata structure is modified,
    update the adb version so that it will invalidate older
    backups and not cause issues on restore. When restoring,
    we currently do not support picking specific partitions.
    It's all or nothing.
    
    Change-Id: Idb92c37fc9801dc8d89ed2a4570e9d12e76facf8

---
+++ orscmd/twrpback.hpp
@@ -0,0 +1,55 @@
+/*		Copyright TeamWin 2015
+		TWRP is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+
+		TWRP is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with TWRP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <string>
+#include <fstream>
+
+#include "orscmd.h"
+#include "../variables.h"
+#include "../twcommon.h"
+
+class twrpback {
+public:
+	twrpback(void);
+	virtual ~twrpback(void);
+	int backup(char *command);
+	int restore(void);
+	void adblogwrite(std::string writemsg);
+
+private:
+	int read_fd;
+	int write_fd;
+	int ors_fd;
+	int adb_control_fd;
+	int adb_read_fd;
+	int adb_write_fd;
+	int breakloop;
+	char result[512];
+	char cmd[512];
+	char operation[512];
+	std::ofstream adblogfile;
+	void adbloginit(void);
+};
