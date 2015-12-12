ebbd08ba4c063539782bc369b2a16b6fa38502aa
bigbiff <bigbiff@teamw.in>
2015-12-12 18:30:21 -0500

    ADB: Add adb backup for TWRP.
    Functionality for client side to backup
    tar and image streams over adbd to the client under backup.ab.
    
    Requires: https://gerrit.omnirom.org/#/c/14677/ cherry-pick.
    Using adb backup on the client side you can backup the partitions
    TWRP knows about.
    
    On the client side you can do the following:
    adb backup -f <filename> --twrp <options> where options are
    --compress: compress data
    system: backup system
    cache: backup cache
    data: backup data
    boot: backup boot
    etc for each partition.
    
    You can string multiple options,
    i.e. adb backup -f <filename> --twrp --compress cache system data
    
    adb backup in TWRP will take any option corresponding
    to TWRP fstab partitions, e.g. efs boot as well.
    
    If you do not specify the filename with the -f option,
    adb will backup your data to a filename backup.ab on the client.
    You can then rename the file and encrypt it with desktop tools.
    
    To restore the backup use the following convention:
    adb restore <filename>
    
    Structures are used to store metadata in binary inside
    of the file itself. If the metadata structure is modified,
    update the adb version so that it will invalidate older
    backups and not cause issues on restore. When restoring,
    we currently do not support picking specific partitions.
    It's all or nothing.
    
    Change-Id: Idb92c37fc9801dc8d89ed2a4570e9d12e76facf8

---
+++ adbbu/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	twrpback.cpp ../twrpDigest.cpp ../digest/md5.c
+LOCAL_SHARED_LIBRARIES += libstdc++ libz
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
+    LOCAL_SHARED_LIBRARIES += libstlport
+else
+	LOCAL_SHARED_LIBRARIES += libc++
+endif
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
+	LOCAL_C_INCLUDES += external/stlport/stlport
+endif   
+LOCAL_C_INCLUDES += bionic external/zlib
+LOCAL_CFLAGS:= -c -W
+LOCAL_MODULE:= twrpbu
+LOCAL_MODULE_STEM := bu
+LOCAL_MODULE_TAGS:= eng
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+include $(BUILD_EXECUTABLE)
