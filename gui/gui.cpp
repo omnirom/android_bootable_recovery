66fc3873fb4b92deabdf9f147e31bbca248cdf9d
bigbiff <bigbiff@teamw.in>
2015-12-12 18:30:21 -0500

    ADB: Add adb backup for TWRP.
    Functionality for client side to backup
    tar and image streams over adbd to the client under backup.ab.
    
    Using adb backup on the client side you can backup the partitions
    TWRP knows about.
    
    On the client side you can do the following:
    adb backup -f <filename> --twrp <options> where options are
    --compress: compress data
    system: backup system
    cache: backup cache
    data: backup data
    boot: backup boot
    etc for each partition.
    
    You can string multiple options,
    i.e. adb backup -f <filename> --twrp --compress cache system data
    
    adb backup in TWRP will take any option corresponding
    to TWRP fstab partitions, e.g. efs boot as well.
    
    If you do not specify the filename with the -f option,
    adb will backup your data to a filename backup.ab on the client.
    You can then rename the file and encrypt it with desktop tools.
    
    To restore the backup use the following convention:
    adb restore <filename>
    
    Structures are used to store metadata in binary inside
    of the file itself. If the metadata structure is modified,
    update the adb version so that it will invalidate older
    backups and not cause issues on restore. When restoring,
    we currently do not support picking specific partitions.
    It's all or nothing.
    
    Change-Id: Idb92c37fc9801dc8d89ed2a4570e9d12e76facf8

--- gui/gui.cpp
+++ gui/gui.cpp
@@ -460,6 +460,13 @@ static void ors_command_read()
 				gui_set_FILE(orsout);
 				PageManager::GetResources()->DumpStrings();
 				ors_command_done();
+			} else if (strlen(command) == 23 && strncmp(command, "adbbackup", 9) == 0) {
+				gui_set_FILE(orsout);
+				DataManager::SetValue("tw_action", "twcmd");
+				DataManager::SetValue("tw_action_param", command);
+				DataManager::SetValue("tw_enable_adb_backup", 1);
+				gui_changePage("backup");
+				ors_command_done();
 			} else {
 				// mirror output messages
 				gui_set_FILE(orsout);
@@ -480,8 +487,6 @@ static void ors_command_read()
 				// put all things that need to be done after the command is finished into ors_command_done, not here
 			}
 		}
-	} else {
-		LOGINFO("ORS command line read returned an error: %i, %i, %s\n", read_ret, errno, strerror(errno));
 	}
 }
 
