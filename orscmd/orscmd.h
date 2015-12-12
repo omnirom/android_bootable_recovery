ab7494f88cffdf6cba491dbbac1bf78e258b235c
bigbiff <bigbiff@teamw.in>
2015-12-12 18:30:21 -0500

    ADB: Add adb backup for TWRP.
    Functionality for client side to backup
    tar and image streams over adbd to the client under backup.ab.
    
    Requires: https://gerrit.omnirom.org/#/c/14677/ cherry-pick.
    Using adb backup on the client side you can backup the partitions
    TWRP knows about.
    
    On the client side you can do the following:
    adb backup <options> where options are
    --compress: compress data
    --system: backup system
    --cache: backup cache
    --data: backup data
    --boot: backup boot
    etc...
    
    You can string multiple options,
    i.e. adb backup --cache --system --data --compress
    
    adb backup in TWRP will take any option corresponding
    to TWRP fstab partitions, e.g. --efs --boot as well.
    
    This will backup your data to a filename backup.ab on the client.
    You can then rename the file and encrypt it with desktop tools.
    To restore the backup use the following convention:
    adb restore <filename>
    
    You may rename the backup file backup.ab to any filename
    preferred.
    
    Structures are used to store metadata in binary inside
    of the file itself. If the metadata structure is modified,
    update the adb version so that it will invalidate older
    backups and not cause issues on restore. When restoring,
    we currently do not support picking specific partitions.
    It's all or nothing.
    
    Change-Id: Idb92c37fc9801dc8d89ed2a4570e9d12e76facf8

--- orscmd/orscmd.h
+++ orscmd/orscmd.h
@@ -18,5 +18,62 @@
 
 #define ORS_INPUT_FILE "/sbin/orsin"
 #define ORS_OUTPUT_FILE "/sbin/orsout"
+#define TW_ADB_BACKUP "/tmp/twadbbackup"
+#define TW_ADB_RESTORE "/tmp/twadbrestore"
+#define TW_ADB_CONTROL "/tmp/twadbcontrol"
+#define TWRP "TWRP"
+#define TWENDADB "twendadb"
+#define TWCNT "twcnt"
+#define TWFN "twfn"
+#define TWIMG "twimg"
+#define TWEOF "tweof"
+#define MD5TRAILER "md5tr"
+#define TWMD5 "twmd5"
+#define ADB_BACKUP_VERSION 0x01
+#define TWERROR "twerror"
+
+//structs for adb backup need to align to 512 bytes for reading 512
+//bytes at a time
+//Each struct contains a crc field so that when we are checking for commands
+//and the crc doesn't match we still assume it's data matching the command
+//struct but not really a command
+
+//generic cmd structure to align fields for sending commands to and from adb backup
+struct twcmd {
+	char start_of_header[6];
+	char type[10];
+	uint32_t crc;
+	char space[492];
+};
+
+//general info for file metadata stored in adb backup header
+struct twfilehdr {
+	char start_of_header[6];
+	char type[10];
+	uint32_t crc;
+	uint64_t size;
+	uint64_t compressed;
+	char name[476];
+};
+
+//md5 for files stored as a trailer to files in the adb backup file to check
+//that they are restored correctly
+struct md5trailer {
+	char start_of_trailer[6];
+	char type[10];
+	uint32_t crc;
+	char md5[33];
+	char space[457];
+};
+
+//info for version and number of partitions backed up
+struct twheader {
+	char start_of_header[6];
+	char type[10];
+	uint32_t crc;
+	uint64_t partition_count;
+	uint64_t version;
+	char space[476];
+};
 
 #endif //__ORSCMD_H
