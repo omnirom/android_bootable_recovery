d5d2cf7233a19d367f94b406e5c46ae975319b73
Ethan Yonker <dees_troy@teamw.in>
2015-12-28 21:54:50 +0100

    WIP: Update minuitwrp graphics in line with latest minui
    Note: events.cpp is still old code renamed to cpp to make it
    easier to call functions like gr_fb_width().
    
    I had to modify AOSP fbdev code to provide a separate memory
    surface for drawing to as drawing directly to the framebuffer
    resulted in rendering taking about 5 times longer.
    
    I also modified AOSP adf code to provide a separate memory surface
    for drawing for the same performance reasons. The Nexus 9 supports
    adf graphics.
    
    Overlay graphics work on at least one device. Overlay provides a
    separate memory buffer already so performance is good.
    
    I do not have a drm device yet that I know of. I made some attempt
    to update the drm code to determine the correct pixel format based
    on the drm graphics format, but what is available in pixel flinger
    and what is available in drm do not line up all that well.
    
    Change-Id: Ibd45bccca6ac2cb826037aa9b2aa5065cf683eed

--- gui/pages.cpp
+++ gui/pages.cpp
@@ -39,11 +39,11 @@
 
 extern "C" {
 #include "../twcommon.h"
-#include "../minuitwrp/minui.h"
 #include "../minzip/SysUtil.h"
 #include "../minzip/Zip.h"
 #include "gui.h"
 }
+#include "../minuitwrp/minui.h"
 
 #include "rapidxml.hpp"
 #include "objects.hpp"
