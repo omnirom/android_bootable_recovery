4ad3a1096674b0ed688035c95d199dee041651af
Ethan Yonker <dees_troy@teamw.in>
2015-12-28 21:54:50 +0100

    Update minuitwrp graphics in line with latest minui
    Note: events.cpp is still old code renamed to cpp to make it
    easier to call functions like gr_fb_width().
    
    I had to modify AOSP fbdev code to provide a separate memory
    surface for drawing to as drawing directly to the framebuffer
    resulted in rendering taking about 5 times longer.
    
    I also modified AOSP adf code to provide a separate memory surface
    for drawing for the same performance reasons. The Nexus 9 supports
    adf graphics.
    
    Overlay graphics work on at least one device. Overlay provides a
    separate memory buffer already so performance is good.
    
    I do not have a drm device yet that I know of. I made some attempt
    to update the drm code to determine the correct pixel format based
    on the drm graphics format, but what is available in pixel flinger
    and what is available in drm do not line up all that well. Reports
    are that the Pixel C is using drm graphics, but performance is
    slow, likely due to the use of a mmap instead of a memory buffyer.
    
    Change-Id: Ibd45bccca6ac2cb826037aa9b2aa5065cf683eed

--- gui/console.cpp
+++ gui/console.cpp
@@ -37,8 +37,8 @@
 
 extern "C" {
 #include "../twcommon.h"
-#include "../minuitwrp/minui.h"
 }
+#include "../minuitwrp/minui.h"
 
 #include "rapidxml.hpp"
 #include "objects.hpp"
--- gui/terminal.cpp
+++ gui/terminal.cpp
@@ -32,8 +32,8 @@
 
 extern "C" {
 #include "../twcommon.h"
-#include "../minuitwrp/minui.h"
 }
+#include "../minuitwrp/minui.h"
 
 #include "rapidxml.hpp"
 #include "objects.hpp"
@@ -869,10 +869,10 @@ void GUITerminal::RenderItem(size_t itemindex, int yPos, bool selected)
 		// render cursor
 		int cursorX = engine->getCursorX();
 		std::string leftOfCursor = line.substr(0, cursorX);
-		int x = gr_measureEx(leftOfCursor.c_str(), mFont->GetResource());
+		int x = gr_ttf_measureEx(leftOfCursor.c_str(), mFont->GetResource());
 		// note that this single character can be a UTF-8 sequence
 		std::string atCursor = (size_t)cursorX < line.length() ? line.substr(cursorX, 1) : " ";
-		int w = gr_measureEx(atCursor.c_str(), mFont->GetResource());
+		int w = gr_ttf_measureEx(atCursor.c_str(), mFont->GetResource());
 		gr_color(mFontColor.red, mFontColor.green, mFontColor.blue, mFontColor.alpha);
 		gr_fill(mRenderX + x, yPos, w, actualItemHeight);
 		gr_color(mBackgroundColor.red, mBackgroundColor.green, mBackgroundColor.blue, mBackgroundColor.alpha);
@@ -890,7 +890,7 @@ void GUITerminal::InitAndResize()
 	// make sure the shell is started
 	engine->initPty();
 	// send window resize
-	int charWidth = gr_measureEx("N", mFont->GetResource());
+	int charWidth = gr_ttf_measureEx("N", mFont->GetResource());
 	engine->setSize(mRenderW / charWidth, GetDisplayItemCount(), mRenderW, mRenderH);
 }
 
