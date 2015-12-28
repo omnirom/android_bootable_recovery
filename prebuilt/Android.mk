f40e6cda7b94803ee7050e17ac747bd873273899
dianlujitao <dianlujitao@gmail.com>
2015-12-28 23:51:19 +0800

    Add zip and unzip when using toybox
    Change-Id: I75500bfe4e7adf66c5538597d79ed0539e7bb551
    Signed-off-by: dianlujitao <dianlujitao@gmail.com>

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -20,6 +20,12 @@ else
 	RELINK_SOURCE_FILES += $(TARGET_OUT_SHARED_LIBRARIES)/libcrypto.so
 	ifneq (,$(filter $(PLATFORM_SDK_VERSION), 23))
 	    RELINK_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/toybox
+	    ifneq ($(wildcard external/zip/Android.mk),)
+                RELINK_SOURCE_FILES += $(TARGET_OUT_OPTIONAL_EXECUTABLES)/zip
+	    endif
+	    ifneq ($(wildcard external/unzip/Android.mk),)
+                RELINK_SOURCE_FILES += $(TARGET_OUT_OPTIONAL_EXECUTABLES)/unzip
+	    endif
 	endif
 endif
 RELINK_SOURCE_FILES += $(TARGET_RECOVERY_ROOT_OUT)/sbin/pigz
