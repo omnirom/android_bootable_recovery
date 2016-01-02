6052389f0bc69d4abe6ce2335f5e6b126b489b81
dianlujitao <dianlujitao@gmail.com>
2016-01-02 12:15:50 +0800

    Generate new format /etc/fstab
     * Toybox's mount can't work with the old-fashioned fstab.
     * Busybox mount is compatible with the new one.
    
    Change-Id: I16b7d6424a4424c2a92d82846a706733bf58f8e1
    Signed-off-by: dianlujitao <dianlujitao@gmail.com>

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -183,7 +183,7 @@ int TWPartitionManager::Write_Fstab(void) {
 	}
 	for (iter = Partitions.begin(); iter != Partitions.end(); iter++) {
 		if ((*iter)->Can_Be_Mounted) {
-			Line = (*iter)->Actual_Block_Device + " " + (*iter)->Mount_Point + " " + (*iter)->Current_File_System + " rw\n";
+			Line = (*iter)->Actual_Block_Device + " " + (*iter)->Mount_Point + " " + (*iter)->Current_File_System + " rw 0 0\n";
 			fputs(Line.c_str(), fp);
 		}
 		// Handle subpartition tracking
