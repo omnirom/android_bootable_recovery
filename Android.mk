370c61be7ba6ff7d76e7ca3d19c133fd6e16d00e
Ethan Yonker <dees_troy@teamw.in>
2016-01-11 22:21:18 -0600

    WIP: Use sgdisk to partition sdcards
    Change-Id: Ib80882d9b5776e5e9358b11340fba392e6f1ae09

--- Android.mk
+++ Android.mk
@@ -374,7 +374,11 @@ ifneq ($(TW_NO_EXFAT), true)
     LOCAL_ADDITIONAL_DEPENDENCIES += mkexfatfs fsckexfat
 endif
 ifeq ($(BOARD_HAS_NO_REAL_SDCARD),)
-    LOCAL_ADDITIONAL_DEPENDENCIES += parted
+    ifeq ($(shell test $(PLATFORM_SDK_VERSION) -gt 22; echo $$?),0)
+        LOCAL_ADDITIONAL_DEPENDENCIES += sgdisk
+    else
+        LOCAL_ADDITIONAL_DEPENDENCIES += sgdisk_static
+    endif
 endif
 ifneq ($(TW_EXCLUDE_ENCRYPTED_BACKUPS), true)
     LOCAL_ADDITIONAL_DEPENDENCIES += openaes ../openaes/LICENSE
