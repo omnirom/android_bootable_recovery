579e907d6023d334a2841fb2384362f6a3c851a5
Xuefer <xuefer@gmail.com>
2016-01-22 13:35:50 +0800

    Android 6.0 needs permission +x to create screenshot files
    Change-Id: Id4a6c04712238b86787696337874239591082d39
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- gui/action.cpp
+++ gui/action.cpp
@@ -928,7 +928,7 @@ int GUIAction::screenshot(std::string arg __unused)
 		strcpy(path, "/tmp/");
 	}
 
-	if(!TWFunc::Create_Dir_Recursive(path, 0666, uid, gid))
+	if(!TWFunc::Create_Dir_Recursive(path, 0775, uid, gid))
 		return 0;
 
 	tm = time(NULL);
