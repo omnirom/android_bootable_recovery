74c317486a1309e4457d1dd1f3b22bd7cf5d13e3
Ethan Yonker <dees_troy@teamw.in>
2016-01-29 16:37:37 -0600

    Add compare xml script and move/update relink-binaries.sh
    Add script to compare string values between 2 language xml files
    which will make it easier to identify strings that are missing
    between the 2 files.
    
    Move relink-binaries.sh to scripts folder and update it to handle
    relinking 64 bit binaries.
    
    Change-Id: Ia7a8bdbeea02256b3b864ab77e15912795275d33

--- scripts/relink-binaries.sh
+++ scripts/relink-binaries.sh
@@ -6,7 +6,7 @@ process_file()
    dst=$1-1 #/$(basename $2)
    cp -f -p $src $dst
 
-   sed "s|/system/bin/linker\x0|/sbin/linker\x0\x0\x0\x0\x0\x0\x0|g" $dst | sed "s|/system/bin/sh\x0|/sbin/sh\x0\x0\x0\x0\x0\x0\x0|g" > $dst-mod
+   sed "s|/system/bin/linker64\x0|/sbin/linker64\x0\x0\x0\x0\x0\x0\x0|g" $dst | sed "s|/system/bin/linker\x0|/sbin/linker\x0\x0\x0\x0\x0\x0\x0|g" | sed "s|/system/bin/sh\x0|/sbin/sh\x0\x0\x0\x0\x0\x0\x0|g" > $dst-mod
    #sed "s|/sbin/linker\x0|/system/bin/linker\x0\x0\x0\x0\x0\x0\x0|g" $dst | sed "s|/sbin/sh\x0|/system/bin/sh\x0\x0\x0\x0\x0\x0\x0|g" > $dst-mod
    rm $dst
 }
