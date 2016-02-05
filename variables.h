b7e8b98ce6641ec5b6a10e91277f3b045e981508
Ethan Yonker <dees_troy@teamw.in>
2016-02-05 14:16:38 -0600

    3.0.0-0
    For better or worse, here we go...
    
    Change-Id: Ic1150b59e4ebf34f06c689b2a1b10ae691a027bd

--- variables.h
+++ variables.h
@@ -17,7 +17,7 @@
 #ifndef _VARIABLES_HEADER_
 #define _VARIABLES_HEADER_
 
-#define TW_VERSION_STR              "2.8.7.0"
+#define TW_VERSION_STR              "3.0.0-0"
 
 #define TW_USE_COMPRESSION_VAR      "tw_use_compression"
 #define TW_FILENAME                 "tw_filename"
