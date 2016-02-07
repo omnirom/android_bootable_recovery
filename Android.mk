07d9a5f06d06ca8ea2dc3d1296406c408c0e8166
nkk71 <nkk71x@gmail.com>
2016-02-07 19:02:25 +0200

    Use restorecon from toolbox

--- Android.mk
+++ Android.mk
@@ -525,6 +525,13 @@ include $(CLEAR_VARS)
 BUSYBOX_LINKS := $(shell cat external/busybox/busybox-full.links)
 exclude := tune2fs mke2fs mkdosfs mkfs.vfat gzip gunzip
 
+#MultiROM uses restorecon -D which is only available in toolbox
+ifeq ($(TARGET_RECOVERY_IS_MULTIROM), true)
+	ifeq ($(TWHAVE_SELINUX), true)
+		exclude += restorecon
+	endif
+endif
+
 # If busybox does not have restorecon, assume it does not have SELinux support.
 # Then, let toolbox provide 'ls' so -Z is available to list SELinux contexts.
 ifeq ($(TWHAVE_SELINUX), true)
