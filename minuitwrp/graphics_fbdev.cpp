961a23be159116c1719918adb2b788a96766f0b9
Muhammad Fahad Baig <muhammad.fb.79@gmail.com>
2016-04-07 09:19:33 +0100

    Black screen patch required for some HiSilicon devices (FBIOPAN_DISPLAY)
    Change-Id: Ib114dcc7b48219142602a2fbc256e2a0258b26e0

--- minuitwrp/graphics_fbdev.cpp
+++ minuitwrp/graphics_fbdev.cpp
@@ -103,6 +103,12 @@ static void set_displayed_framebuffer(unsigned n)
     vi.bits_per_pixel = gr_framebuffer[0].pixel_bytes * 8;
     if (ioctl(fb_fd, FBIOPUT_VSCREENINFO, &vi) < 0) {
         perror("active fb swap failed");
+#ifdef TW_FBIOPAN
+    } else {
+        if (ioctl(fb_fd, FBIOPAN_DISPLAY, &vi) < 0) {
+            perror("pan failed");
+        }
+#endif
     }
     displayed_buffer = n;
 }
