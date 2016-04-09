21ff5ee42ad1a6b94c2f8ae31409382eb0f6bc47
nkk71 <nkk71x@gmail.com>
2016-04-09 14:36:12 +0300

    Workaround (v2) for no kexec-hardboot kernels
    * MultiROM support for no kexec-hardboot enabled kernels.
      It has been tested on the HTC One M7, M8 and M9 successfully.
    
    * This commit is the TWRP recovery portion, which adds
      (1) various settings in TWRP->MultiROM-Settings
      (2) properly restores primary kernel if needed
      the real code is in multirom https://github.com/nkk71/multirom
    
    * This is an updated and more reliable version of the initial
      WIP https://github.com/nkk71/multirom/commit/6ae88fe6590d99844e87b31286db19f133ea70e8
    
    Note:
    * The following build flag needs to be added in BoardConfig:
      MR_ALLOW_NKK71_NOKEXEC_WORKAROUND := true

--- multirom/multirom.h
+++ multirom/multirom.h
@@ -116,6 +116,9 @@ public:
 		int auto_boot_seconds;
 		int auto_boot_type;
 		std::string auto_boot_rom;
+#ifdef MR_ALLOW_NKK71_NOKEXEC_WORKAROUND
+		int allow_nkk71_nokexec;
+#endif
 		int colors;
 		int brightness;
 		int enable_adb;
@@ -132,6 +135,9 @@ public:
 	static bool folderExists();
 	static std::string getRomsPath();
 	static std::string getPath();
+#ifdef MR_ALLOW_NKK71_NOKEXEC_WORKAROUND
+	static void nokexec_restore_primary_and_cleanup();
+#endif
 	static int getType(std::string name);
 	static std::string listRoms(uint32_t mask = MASK_ALL, bool with_bootimg_only = false);
 	static void setInstaller(MROMInstaller *i);
