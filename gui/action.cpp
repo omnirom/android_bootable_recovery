6b0586c74238d1b30fef11dd0fde61460488d566
Matt Mower <mowerm@gmail.com>
2016-05-27 12:37:37 -0500

    GUI: Remove delay before auto-reboot after install
    Change-Id: Ib5e1447918adfde45eba361c088800812b690890

--- gui/action.cpp
+++ gui/action.cpp
@@ -1026,10 +1026,6 @@ int GUIAction::flash(std::string arg)
 
 	reinject_after_flash();
 	PartitionManager.Update_System_Details();
-	if (DataManager::GetIntValue("tw_install_reboot") > 0 && ret_val == 0) {
-		gui_msg("install_reboot=Rebooting in 5 seconds");
-		usleep(5000000);
-	}
 	operation_end(ret_val);
 	// This needs to be after the operation_end call so we change pages before we change variables that we display on the screen
 	DataManager::SetValue(TW_ZIP_QUEUE_COUNT, zip_queue_index);
