7f2e68e100824b53e6e8eb8143efe02fa09dae41
nailyk <nailyk_launchpad@nailyk.fr>
2016-08-19 13:27:12 +0200

    [WIP]GUI: add possibility to add device specific stuff
    - Adding a TAB  to make the devicespecific pages displayable
    - Adding a new flag "TW_INCLUDE_DEVICESPECIFIC"
    - Adding a new xml "Devicespecific.xml" wich can be overwritten by the dev
    - Adding a flag that overwrites devicespecific.xml "TW_DEVICESPECIFIC_PATH"
    - [To-Do] find a way to introduce devicespecific languages
    
    So this commit allows the Device Maintainer to add some Devicespecific Stuff to his
    device without actually change the other Devices behaviours. This way we can include
    zips into the twrp that get executed into devices with a big recovery partition
    and giving a way to execute them.
    
    Change-Id: Ia9bf898006da7e4f4ee6a942584fe2dd3c88f8ba

--- gui/pages.cpp
+++ gui/pages.cpp
@@ -1307,6 +1307,10 @@ void PageManager::LoadLanguage(string filename) {
 	else
 		actual_filename = TWRES "languages/" + filename + ".xml";
 	char* xmlFile = PageManager::LoadFileToBuffer(actual_filename, NULL);
+	if (TWFunc::Path_Exists(TWRES "devicelanguages/" + filename + ".xml"))
+	actual_filename = TWRES "devicelanguages/" + filename + ".xml";
+	char* xmlFile = PageManager::LoadFileToBuffer(actual_filename, NULL);
+	else
 	if (xmlFile == NULL)
 		LOGERR("Unable to load '%s'\n", actual_filename.c_str());
 	else {
