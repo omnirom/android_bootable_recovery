bea69c87d6eba0bc77cccc02ae00f41f1514b20b
bigbiff bigbiff <bigbiff@teamw.in>
2016-08-19 17:43:45 -0400

    twrpDigest refactor
    This patch is to refactor twrpDigest using polymorphism
    and inheritance to use the same call patterns for creating and
    reading a digest. Now a library.
    Use SHA2 from libcrypto. SHA2 is default if device has libcrypto.
    
    Change string MD5 everywhere to use digest or Digest instead. Updated
    string tags to digest. Translation will be required.
    
    SHA2 is better for digest creation due to decreased collision space
    compared to MD5 and SHA1.
    
    See https://en.wikipedia.org/wiki/SHA-2
    
    Change-Id: I74b5546789990b12aa4ce2e389d25f80a3fe213f

---
+++ twrpDigest/twrpDigest.cpp
@@ -0,0 +1,57 @@
+/*
+	Copyright 2012 to 2016 TeamWin
+	This file is part of TWRP/TeamWin Recovery Project.
+
+	TWRP is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	TWRP is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with TWRP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <libgen.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <fstream>
+#include "twrpDigest.hpp"
+
+std::string twrpDigest::hexify(uint8_t* hash, size_t len) {
+	char hex[3];
+	std::string digest_string;
+
+	for (size_t i = 0; i < len; ++i) {
+		snprintf(hex, 3, "%02x", hash[i]);
+		digest_string += hex;
+	}
+	return digest_string;
+}
+
+bool twrpDigest::read_file(std::string digest_filename) {
+        char buf[1024];
+        int bytes;
+	int fd;
+
+	fd = open(digest_filename.c_str(), O_RDONLY);
+        if (fd < 0) {
+                return false;
+        }
+        while ((bytes = read(fd, &buf, sizeof(buf))) != 0) {
+                update((unsigned char*)buf, bytes);
+        }
+        close(fd);
+	return true;
+}
