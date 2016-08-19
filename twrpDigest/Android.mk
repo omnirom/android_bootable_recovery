cd564bfd2a5ebf85581dcbef3d6be7591b4fc501
bigbiff bigbiff <bigbiff@teamw.in>
2016-08-19 17:43:45 -0400

    twrpDigest refactor
    This patch is to refactor twrpDigest using polymorphism
    and inheritance to use the same call patterns for creating and
    reading a digest. Now a library.
    Use SHA2 from libcrypto. SHA2 is default if device has libcrypto.
    
    Change string MD5 everywhere to use digest or Digest instead. Updated
    string tags to digest. Translation will be required.
    
    Change-Id: Id85ed5c796cf0ce6f79ccd0500f575c2057bde0c

---
+++ twrpDigest/Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libtwrpdigest
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS = -fno-strict-aliasing
+LOCAL_C_INCLUDES := external/openssl/include 
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
+	LOCAL_C_INCLUDES += external/stlport/stlport
+endif
+
+LOCAL_SHARED_LIBRARIES += libc libstdc++ libcrypto
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
+	LOCAL_SHARED_LIBRARIES += libstlport
+else
+	LOCAL_SHARED_LIBRARIES += libc++
+endif
+
+LOCAL_SRC_FILES = \
+	twrpDigest.cpp \
+	twrpMD5.cpp \
+	twrpSHA.cpp \
+	digest/md5/md5.c \
+
+include $(BUILD_SHARED_LIBRARY)
