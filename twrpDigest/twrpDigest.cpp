e14735ad1181790e4897d00ff13a0a8f957578f4
bigbiff bigbiff <bigbiff@teamw.in>
2016-08-19 17:43:45 -0400

    twrpDigest refactor
    This patch is to refactor twrpDigest using polymorphism
    and inheritance to use the same call patterns for creating and
    reading a digest. Now a library. Implemented SHA2 from
    http://create.stephan-brumme.com/hash-library/
    Change MD5 everywhere to use digest or Digest instead. Updated
    string tags to digest. Translation will be required.
    
    Change-Id: Id85ed5c796cf0ce6f79ccd0500f575c2057bde0c

---
+++ twrpDigest/twrpDigest.cpp
@@ -0,0 +1,34 @@
+/*
+	Copyright 2012 to 2016 TeamWin
+	This file is part of TWRP/TeamWin Recovery Project.
+
+	TWRP is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	TWRP is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with TWRP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <libgen.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <fstream>
+#include "twrpDigest.hpp"
+
+void twrpDigest::set_filename(std::string& filename) {
+	digest_filename = filename;
+}
