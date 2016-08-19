4a6825b07a34f92f23ce21ec1d0c0e26677833a6
bigbiff bigbiff <bigbiff@teamw.in>
2016-08-19 17:43:45 -0400

    twrpDigest refactor
    This patch is to refactor twrpDigest using polymorphism
    and inheritance to use the same call patterns for creating and
    reading a digest. Now a library.
    Use SHA2 from libcrypto. SHA2 is default if device has libcrypto.
    
    Change string MD5 everywhere to use digest or Digest instead. Updated
    string tags to digest. Translation will be required.
    
    SHA2 is better for digest creation due to decreased collision space
    compared to MD5 and SHA1.
    
    See https://en.wikipedia.org/wiki/SHA-2
    
    Change-Id: I74b5546789990b12aa4ce2e389d25f80a3fe213f

---
+++ twrpDigest/twrpDigest.hpp
@@ -0,0 +1,37 @@
+/*
+        Copyright 2012 to 2017 TeamWin
+        This file is part of TWRP/TeamWin Recovery Project.
+
+        TWRP is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        TWRP is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with TWRP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __TWRPDIGEST_H
+#define __TWRPDIGEST_H
+
+class twrpDigest {
+public:
+	twrpDigest() {};
+	virtual ~twrpDigest() {};
+	bool read_file(std::string digest_filename);                     // Read file into digest algorithm
+	virtual bool update(const unsigned char* stream, size_t len) = 0;         // Update the digest with new data
+	virtual std::string return_digest_string(void) = 0;              // Returns the digest of the file as a string instead of the raw format
+	virtual bool verify_digest(std::string digest) = 0;              // Verify the digest of the file against the passed string
+
+protected:
+	virtual void init(void) = 0;                                     // Initialize the digest according to the algorithm
+	virtual void finalize(void) = 0;                                 // Finalize the digest input for creating the final digest
+	std::string hexify(uint8_t* hash, size_t len);                   // Take an 8 bit byte and turn it into a hex string
+};
+
+#endif //__TWRPDIGEST_H
