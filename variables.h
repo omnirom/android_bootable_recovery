c4fc53cfa4e1094e11ed94f541de724fd0fe6129
SdtBarbarossa <DaMicha_essen@gmx.de>
2016-08-19 13:27:12 +0200

    [GUI] add possibility to add device specific stuff
    - Adding a TAB  to make the devicespecific pages displayable
    - Adding a new xml "Devicespecific.xml" wich can be overwritten by the dev
    - Adding a flag that overwrites devicespecific.xml "TW_DEVICESPECIFIC_RES_PATH"
    - If you want to use Multilanguage use the already existing customlanguages
    
    So this commit allows the Device Maintainer to add some Devicespecific Stuff to his
    device without actually change the other Devices behaviours. This way we can include
    zips into the twrp that get executed into devices with a big recovery partition
    and giving a way to execute them.
    
    Change-Id: Ia9bf898006da7e4f4ee6a942584fe2dd3c88f8ba

--- variables.h
+++ variables.h
@@ -135,6 +135,7 @@
 #define TW_CRYPTO_PASSWORD          "tw_crypto_password"
 #define TW_SDEXT_DISABLE_EXT4       "tw_sdext_disable_ext4"
 #define TW_MILITARY_TIME            "tw_military_time"
+#define TW_SHOW_DEVICESPECIFIC_RES  "tw_show_devicespecific"
 
 // Also used:
 //   tw_boot_is_mountable
