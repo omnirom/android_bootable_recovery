25f3789cdf6d80b865ca908c686f8fec422e57dd
bigbiff bigbiff <bigbiff@teamw.in>
2016-08-19 17:43:45 -0400

    twrpDigest refactor
    This patch is to refactor twrpDigest using polymorphism
    and inheritance to use the same call patterns for creating and
    reading a digest. Now a library. Implemented SHA2 from
    http://create.stephan-brumme.com/hash-library/
    Change MD5 everywhere to use digest or Digest instead. Updated
    string tags to digest. Translation will be required.
    
    Change-Id: Id85ed5c796cf0ce6f79ccd0500f575c2057bde0c

---
+++ twrpDigest/twrpSHA2.hpp
@@ -0,0 +1,36 @@
+/*
+        Copyright 2012 to 2016 bigbiff/Dees_Troy TeamWin
+        This file is part of TWRP/TeamWin Recovery Project.
+
+        TWRP is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        TWRP is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with TWRP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "digest/sha2/sha256.h"
+
+
+class twrpSHA2: public twrpDigest {
+public:
+        void init_digest(void);
+        bool update_stream(unsigned char* stream, int len);
+        void finalize_stream(void);
+        std::string create_digest_string(void);
+        std::string return_digest_string(void);
+        bool verify_digest(std::string digest);
+
+private:
+	SHA256 sha2_hash;
+	SHA256 sha2_stream;
+	std::string sha2verify;
+	std::string sha2file;
+};
