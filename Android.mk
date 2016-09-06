623f741c7c3c13b6cb291d155a139ec1df3ed284
bigbiff bigbiff <bigbiff@teamw.in>
2016-09-05 21:04:51 -0400

    ADB Backup: add ability for TWRP GUI to restore
    Restore adb backup files that TWRP made to your PC.
    Put files in your backup directory to see them.
    
    e.g. /sdcard/TWRP/BACKUPS/<sn>
    
    Change-Id: I2c57970d77b64c39a302159041456e761c185259

--- Android.mk
+++ Android.mk
@@ -60,7 +60,8 @@ LOCAL_SRC_FILES := \
     twinstall.cpp \
     twrp-functions.cpp \
     openrecoveryscript.cpp \
-    tarWrite.c
+    tarWrite.c \
+    twrpAdbBuFifo.cpp
 
 ifneq ($(TARGET_RECOVERY_REBOOT_SRC),)
   LOCAL_SRC_FILES += $(TARGET_RECOVERY_REBOOT_SRC)
