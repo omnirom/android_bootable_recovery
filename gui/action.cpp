b09ad93df6b5bd1f19c6bde5940e398c09c1a8e4
nkk71 <nkk71x@gmail.com>
2016-09-09 23:25:28 +0300

    MultiROM: fix compile warnings 'invalid suffix on literal'
    Change-Id: I3ff8e7bdb52b5f1df3a2dccc5cfee9fca3d8a9f7

--- gui/action.cpp
+++ gui/action.cpp
@@ -2614,7 +2614,7 @@ int GUIAction::system_image_upgrader(std::string arg __unused)
 	if(TWFunc::Path_Exists(UBUNTU_COMMAND_FILE))
 	{
 		gui_print("\n");
-		res = TWFunc::Exec_Cmd_Show_Output("system-image-upgrader "UBUNTU_COMMAND_FILE);
+		res = TWFunc::Exec_Cmd_Show_Output("system-image-upgrader " UBUNTU_COMMAND_FILE);
 		gui_print("\n");
 
 		if(res != 0)
@@ -2624,7 +2624,7 @@ int GUIAction::system_image_upgrader(std::string arg __unused)
 		}
 		DataManager::SetValue("system-image-upgrader-res", res);
 	} else
-		gui_print("Could not find system-image-upgrader command file: "UBUNTU_COMMAND_FILE"\n");
+		gui_print("Could not find system-image-upgrader command file: " UBUNTU_COMMAND_FILE"\n");
 
 	DataManager::SetValue("tw_page_done", 1);
 	operation_end(res);
--- multirom/multirom.cpp
+++ multirom/multirom.cpp
@@ -191,8 +191,8 @@ bool MultiROM::setRomsPath(std::string loc)
 			LOGERR("Failed to mount location \"%s\"!\n", lnk_path.c_str());
 			return false;
 		}
-		m_curr_roms_path = "/mnt/multirom-"TARGET_DEVICE"/";
-		mkdir("/mnt/multirom-"TARGET_DEVICE"/", 0777);
+		m_curr_roms_path = "/mnt/multirom-" TARGET_DEVICE "/";
+		mkdir("/mnt/multirom-" TARGET_DEVICE "/", 0777);
 		PartitionManager.Update_tw_multirom_variables(partition);
 		return true;
 	}
@@ -256,8 +256,8 @@ bool MultiROM::setRomsPath(std::string loc)
 		return false;
 	}
 
-	m_curr_roms_path = "/mnt/multirom-"TARGET_DEVICE"/";
-	mkdir("/mnt/multirom-"TARGET_DEVICE"/", 0777);
+	m_curr_roms_path = "/mnt/multirom-" TARGET_DEVICE "/";
+	mkdir("/mnt/multirom-" TARGET_DEVICE "/", 0777);
 	PartitionManager.Update_tw_multirom_variables(partition);
 	return true;
 }
@@ -1060,14 +1060,14 @@ exit:
 	if(hacker.getProcessFlags() & EDIFY_BLOCK_UPDATES)
 		failsafeCheckPartition("/tmp/mrom_fakesyspart");
 
-	if(restore_script && hacker.restoreState() && hacker.writeToFile("/tmp/"MR_UPDATE_SCRIPT_NAME))
+	if(restore_script && hacker.restoreState() && hacker.writeToFile("/tmp/" MR_UPDATE_SCRIPT_NAME))
 	{
 		gui_print("Restoring original updater-script\n");
 		if(system_args("cd /tmp && zip \"%s\" %s", file.c_str(), MR_UPDATE_SCRIPT_NAME) != 0)
 			LOGERR("Failed to restore original updater-script, THIS ZIP IS NOW UNUSEABLE FOR NON-MULTIROM FLASHING\n");
 	}
 
-	system("rm -r "MR_UPDATE_SCRIPT_PATH);
+	system("rm -r " MR_UPDATE_SCRIPT_PATH);
 	if(file == "/tmp/mr_update.zip")
 		system("rm /tmp/mr_update.zip");
 
@@ -1119,14 +1119,14 @@ bool MultiROM::flashORSZip(std::string file, int *wipe_cache)
 		failsafeCheckPartition("/tmp/mrom_fakesyspart");
 	}
 
-	if(restore_script && hacker.restoreState() && hacker.writeToFile("/tmp/"MR_UPDATE_SCRIPT_NAME))
+	if(restore_script && hacker.restoreState() && hacker.writeToFile("/tmp/" MR_UPDATE_SCRIPT_NAME))
 	{
 		gui_print("Restoring original updater-script\n");
 		if(system_args("cd /tmp && zip \"%s\" %s", file.c_str(), MR_UPDATE_SCRIPT_NAME) != 0)
 			LOGERR("Failed to restore original updater-script, THIS ZIP IS NOW UNUSEABLE FOR NON-MULTIROM FLASHING\n");
 	}
 
-	system("rm -r "MR_UPDATE_SCRIPT_PATH);
+	system("rm -r " MR_UPDATE_SCRIPT_PATH);
 	if(file == "/tmp/mr_update.zip")
 		system("rm /tmp/mr_update.zip");
 
@@ -1196,7 +1196,7 @@ bool MultiROM::prepareZIP(std::string& file, EdifyHacker *hacker, bool& restore_
 	script_entry = mzFindZipEntry(&zip, MR_UPDATE_SCRIPT_NAME);
 	if(!script_entry)
 	{
-		gui_print("Failed to find entry "MR_UPDATE_SCRIPT_NAME" in ZIP file %s!\n", file.c_str());
+		gui_print("Failed to find entry " MR_UPDATE_SCRIPT_NAME " in ZIP file %s!\n", file.c_str());
 		goto exit;
 	}
 
@@ -1221,7 +1221,7 @@ bool MultiROM::prepareZIP(std::string& file, EdifyHacker *hacker, bool& restore_
 	hacker->saveState();
 	hacker->replaceOffendings();
 
-	if(!hacker->writeToFile("/tmp/"MR_UPDATE_SCRIPT_NAME))
+	if(!hacker->writeToFile("/tmp/" MR_UPDATE_SCRIPT_NAME))
 		goto exit;
 
 	hacker->writeToFile("/tmp/mrom_last_updater_script");
