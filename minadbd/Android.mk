e6f6d08f0761c9c313fc82447600d931926ee956
sndnvaps <sndnvaps@gmail.com>
2016-10-04 14:05:58 +0800

    libminiadbd: link libcrypto_utils.so to libminiadbd.so
    Change-Id: Idd11194fe123ab59a10a594c54681b41180b2f9e
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- minadbd/Android.mk
+++ minadbd/Android.mk
@@ -30,7 +30,8 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 24; echo $$?),0)
     LOCAL_SHARED_LIBRARIES += libmincrypttwrp
     LOCAL_CFLAGS += -DUSE_MINCRYPT
 else
-    LOCAL_SHARED_LIBRARIES += libcrypto
+    LOCAL_C_INCLUDES += system/core/libcrypto_utils/include
+    LOCAL_SHARED_LIBRARIES += libcrypto libcrypto_utils
 endif
 
 include $(BUILD_SHARED_LIBRARY)
