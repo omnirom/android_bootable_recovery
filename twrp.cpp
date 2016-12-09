805ab3fc8a8cae7937cd0e7cf3c07bacd971c266
bigbiff bigbiff <bigbiff@teamw.in>
2016-12-08 21:21:17 -0500

    TWRP device version string
    For building through jenkins. Export a variable to the shell
    so that we don't monkey patch variables.h and keep the git tag
    clean in the version string.
    Jenkins will export the variable TW_DEVICE_VERSION=n where n > 0.
    The makefile will use this variable to show the device string
    in twrp startup. For mass production builds, omit the
    environment variable to default to 0.
    
    Change-Id: I0d6eb764255d7069c0fb4a378522a009cfe4054f

--- twrp.cpp
+++ twrp.cpp
@@ -108,7 +108,7 @@ int main(int argc, char **argv) {
 	property_set("ro.twrp.version", TW_VERSION_STR);
 
 	time_t StartupTime = time(NULL);
-	printf("Starting TWRP %s-%s on %s (pid %d)\n", TW_VERSION_STR, TW_GIT_REVISION, ctime(&StartupTime), getpid());
+	printf("Starting TWRP %s.%s on %s (pid %d)\n", TW_VERSION_STR, TW_GIT_REVISION, ctime(&StartupTime), getpid());
 
 	// Load default values to set DataManager constants and handle ifdefs
 	DataManager::SetDefaultValues();
