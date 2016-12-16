b4bff5e9d1f514063aaef9ffad870fe13ed70bfb
Ethan Yonker <dees_troy@teamw.in>
2016-12-16 07:47:58 -0600

    Add TWRP app install via TWRP recovery
    Note: I will have to add a build flag for excluding the app later for
    watches and maybe Android TV. I will also have to add support in the
    tablet and watch layouts later. I will merge this for the initial roll out
    to select devices and finish up later.
    
    Change-Id: Ia4ce5522fae542afa1539b10c0691315392a19ab

--- data.cpp
+++ data.cpp
@@ -827,6 +827,16 @@ void DataManager::SetDefaultValues()
 	mConst.SetValue("tw_has_boot_slots", "0");
 #endif
 
+#ifdef TW_OEM_BUILD
+	LOGINFO("TW_OEM_BUILD := true\n");
+	mConst.SetValue("tw_oem_build", "1");
+#else
+	mConst.SetValue("tw_oem_build", "0");
+	mPersist.SetValue("tw_app_prompt", "1");
+	mPersist.SetValue("tw_app_install_system", "1");
+	mData.SetValue("tw_app_install_status", "0"); // 0 = no status, 1 = not installed, 2 = already installed
+#endif
+
 	pthread_mutex_unlock(&m_valuesLock);
 }
 
