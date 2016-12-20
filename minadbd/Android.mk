22eb8992192442880665c8399f43817ea5678e7e
Antonio Jesús <ajmtrz@gmail.com>
2016-12-20 01:30:49 +0100

    fixes to build with cm-14.1
    Change-Id: I68b88cc9fb857f32864556c4b6c9c8e6ee744051

--- minadbd/Android.mk
+++ minadbd/Android.mk
@@ -30,7 +30,7 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 24; echo $$?),0)
     LOCAL_SHARED_LIBRARIES += libmincrypttwrp
     LOCAL_CFLAGS += -DUSE_MINCRYPT
 else
-    LOCAL_SHARED_LIBRARIES += libcrypto
+    LOCAL_SHARED_LIBRARIES += libcrypto libcrypto_utils
 endif
 
 include $(BUILD_SHARED_LIBRARY)
