a8d72bc3b4087632e8cdad95e8355a2495e15690
Tao Bao <tbao@google.com>
2016-12-25 18:46:50 -0800

    recovery: Drop the "--stages" / '-g' argument.
    This was introduced in commit c87bab101893e8322b49d7c8600e3367b20ab50a.
    But the stage info should be passed through BCB only (there's a
    dedicated field in struct bootloader_message).
    
    This CL removes it from recovery arguments, and also moves 'stage'
    variable to std::string.
    
    Test: 'stage' variable is not used by any device-specific recovery code.
    Test: Code search shows no hit of '--stages' use.
    Change-Id: Iccbde578a13255f2b55dd4a928e9ecf487f16b97

--- common.h
+++ common.h
@@ -29,7 +29,7 @@ extern RecoveryUI* ui;
 extern bool modified_flash;
 
 // The current stage, e.g. "1/2".
-extern const char* stage;
+extern std::string stage;
 
 // The reason argument provided in "--reason=".
 extern const char* reason;
