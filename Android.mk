8a8a07bb76e6e1b36bc613cc12a32d2d2fa2a386
nkk71 <nkk71x@gmail.com>
2017-01-05 23:55:05 +0200

    crypto: Use system's vold for decryption
      If TWRP crypto fails to decrypt partition mount system and use vold
      to attempt decryption.
      This provides a fallback for proprietary OEM encryption as well as
      encryption methods which TWRP hasn't been updated for.
    
      Requirements in device tree:
      * 'TW_CRYPTO_USE_SYSTEM_VOLD := true' in BoardConfig
      * fstab.<hardware> in device/recovery/root
    
      Thanks to @Captain_Throwback for co-authoring and testing.
    
    Tested successfully on HTC devices:
    M8 (KK through MM), M9 (MM and N), A9 (N), 10 (N), Bolt (N),
    Desire 626s (MM), U Ultra (N)
    
    Change-Id: I4d22ab55baf6a2a50adde2e4c1c510c142714227

--- Android.mk
+++ Android.mk
@@ -298,6 +298,10 @@ ifeq ($(TW_INCLUDE_CRYPTO), true)
         LOCAL_CFLAGS += -DTW_INCLUDE_FBE
         LOCAL_SHARED_LIBRARIES += libe4crypt
     endif
+    ifeq ($(TW_CRYPTO_USE_SYSTEM_VOLD), true)
+        LOCAL_CFLAGS += -DTW_CRYPTO_USE_SYSTEM_VOLD
+        LOCAL_STATIC_LIBRARIES += libvolddecrypt
+    endif
 endif
 WITH_CRYPTO_UTILS := \
     $(if $(wildcard system/core/libcrypto_utils/Android.mk),true)
@@ -689,6 +693,9 @@ ifeq ($(TW_INCLUDE_CRYPTO), true)
     include $(commands_recovery_local_path)/crypto/scrypt/Android.mk
     ifeq ($(TW_INCLUDE_CRYPTO_FBE), true)
         include $(commands_recovery_local_path)/crypto/ext4crypt/Android.mk
+    endif
+    ifeq ($(TW_CRYPTO_USE_SYSTEM_VOLD), true)
+        include $(commands_recovery_local_path)/crypto/vold_decrypt/Android.mk
     endif
     include $(commands_recovery_local_path)/gpt/Android.mk
 endif
