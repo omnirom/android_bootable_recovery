4178ccb9379e7ba7d78ab2a83b7fa401764bb842
nkk71 <nkk71x@gmail.com>
2017-01-05 23:55:05 +0200

    crypto: Use system's vold for decryption
      If TWRP crypto fails to decrypt partition mount system and use vold
      to attempt decryption.
      This provides a fallback for proprietary OEM encryption as well as
      encryption methods which TWRP hasn't been updated for.
    
      Requirements in device tree:
      * fstab.{ro.hardware} in device/recovery/root
        The fstab does not need to be complete, but is does need the
        data partition and the encryption entries.
    
      * 'TW_CRYPTO_USE_SYSTEM_VOLD := true' in BoardConfig
      or
      * 'TW_CRYPTO_USE_SYSTEM_VOLD := <list of services>'
    
      Notes:
      * Setting the flag to 'true' will just use system's vdc+vold
        or
      * Setting the flag with additional services, will also start them
        prior to attempting vdc+vold decryption, eg: for qualcomm based
        devices you usually need 'TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd'
    
      * For each service listed an additional import will be automatically
        added to the vold_decrypt.rc file in the form of
        init.recovery.vold_decrypt.{service}.rc
        You will need to add any not already existing .rc files in
        your device/recovery/root folder.
    
      * The service names specified in the vold_decrypt.{service}.rc files
        have to be named 'sys_{service}'
        eg: 'service sys_qseecomd /system/bin/qseecomd'
    
      * Any service already existing in TWRP as {service} or sbin{service} will
        be stopped and restarted as needed.
    
      * You can override the default init.recovery.vold_decrypt.rc file(s)
        by placing same named ones in your device/recovery/root folder.
        If you do, you'll need to manually add the needed imports.
    
    Thanks to @Captain_Throwback for co-authoring and testing.
    
    Tested successfully on HTC devices:
    M8 (KK through MM), M9 (MM and N), A9 (N), 10 (N), Bolt (N),
    Desire 626s (MM), U Ultra (N)
    
    And by Nikolay Jeliazkov on: Xiaomi Mi Max
    
    Change-Id: I4d22ab55baf6a2a50adde2e4c1c510c142714227

---
+++ crypto/vold_decrypt/vold_decrypt.h
@@ -0,0 +1,26 @@
+/*
+    Copyright 2017 TeamWin
+    This file is part of TWRP/TeamWin Recovery Project.
+
+    TWRP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TWRP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TWRP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _VOLD_DECRYPT_H
+#define _VOLD_DECRYPT_H
+
+#include <string>
+
+int vold_decrypt(std::string Password);
+
+#endif // _VOLD_DECRYPT_H
