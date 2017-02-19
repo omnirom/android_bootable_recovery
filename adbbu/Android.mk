13894fec097bc30f9b22618792d74f69053b5595
Matt Mower <mowerm@gmail.com>
2017-02-18 20:58:58 -0600

    Cleanup ADB BU makefile
    Change-Id: Icfc9b637fdc9d1cb6710f6c571ab7ae8964edd1c

--- adbbu/Android.mk
+++ adbbu/Android.mk
@@ -1,42 +1,72 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= \
-	twrpback.cpp \
-	../twrpDigest.cpp \
-	../digest/md5.c
-LOCAL_SHARED_LIBRARIES += libstdc++ libz libselinux
+LOCAL_SRC_FILES := \
+    ../digest/md5.c \
+    ../twrpDigest.cpp \
+    twrpback.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libselinux \
+    libstdc++ \
+    libz
+
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
-    LOCAL_C_INCLUDES += external/stlport/stlport
     LOCAL_SHARED_LIBRARIES += libstlport
 else
     LOCAL_SHARED_LIBRARIES += libc++
 endif
 
-LOCAL_C_INCLUDES += bionic external/zlib
-LOCAL_CFLAGS:= -c -W
-LOCAL_MODULE:= twrpbu
+LOCAL_C_INCLUDES := \
+    bionic \
+    external/zlib
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
+    LOCAL_C_INCLUDES += external/stlport/stlport
+endif
+
+LOCAL_CFLAGS := \
+    -c \
+    -W
+
+LOCAL_MODULE := twrpbu
 LOCAL_MODULE_STEM := bu
-LOCAL_MODULE_TAGS:= eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libtwadbbu
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64 -fno-strict-aliasing
-LOCAL_C_INCLUDES += bionic external/zlib
+
+LOCAL_SRC_FILES := \
+    libtwadbbu.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libc \
+    libstdc++ \
+    libz
+
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
-    LOCAL_C_INCLUDES += external/stlport/stlport
     LOCAL_SHARED_LIBRARIES += libstlport
 else
     LOCAL_SHARED_LIBRARIES += libc++
 endif
 
-LOCAL_SRC_FILES = \
-    libtwadbbu.cpp
+LOCAL_C_INCLUDES := \
+    bionic \
+    external/zlib
 
-LOCAL_SHARED_LIBRARIES += libz libc libstdc++
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
+    LOCAL_C_INCLUDES += external/stlport/stlport
+endif
+
+LOCAL_CFLAGS := \
+    -D_FILE_OFFSET_BITS=64 \
+    -fno-strict-aliasing
+
+LOCAL_MODULE := libtwadbbu
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
