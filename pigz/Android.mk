30fe2b678d98b3941d5cdc6db8cf23d22d1bb6da
Matt Mower <mowerm@gmail.com>
2017-02-18 21:02:23 -0600

    Cleanup pigz makefile
    Change-Id: I89ecf9b6eec091c595d856df2595a94337c329b5

--- pigz/Android.mk
+++ pigz/Android.mk
@@ -2,20 +2,26 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := pigz
-LOCAL_MODULE_TAGS := eng optional
-LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
-LOCAL_CFLAGS :=
-LOCAL_SRC_FILES = pigz.c yarn.c
-LOCAL_C_INCLUDES += $(LOCAL_PATH) \
-					external/zlib
-LOCAL_SHARED_LIBRARIES += libz libc
+LOCAL_SRC_FILES := \
+    pigz.c \
+    yarn.c
+
+LOCAL_C_INCLUDES := \
+    external/zlib
+
+LOCAL_SHARED_LIBRARIES := \
+    libc \
+    libz
 
 LOCAL_POST_INSTALL_CMD := \
     $(hide) mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/sbin && \
     ln -sf pigz $(TARGET_RECOVERY_ROOT_OUT)/sbin/gunzip && \
     ln -sf pigz $(TARGET_RECOVERY_ROOT_OUT)/sbin/gzip && \
     ln -sf pigz $(TARGET_RECOVERY_ROOT_OUT)/sbin/unpigz
+
+LOCAL_MODULE := pigz
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 
 include $(BUILD_EXECUTABLE)
