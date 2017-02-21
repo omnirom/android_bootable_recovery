39ec7d5483233fa7d9403a5662bea90c0e69eaec
Matt Mower <mowerm@gmail.com>
2017-02-21 14:53:19 -0600

    Create general sleep_and_reboot page
    * Separate sleep-and-reboot routine into its own page for use by more
      than just install-and-reboot. We can take advantage of this for ORS.
    * Do not reset user-configurable option tw_install_reboot if cancel is
      pressed on sleep-and-reboot page.
    
    Change-Id: I7d2dddad329a84791210f7952db4f64f4de7f87f

--- twrp.cpp
+++ twrp.cpp
@@ -297,6 +297,10 @@ int main(int argc, char **argv) {
 	if (crash_counter == 0)
 		TWFunc::Fixup_Time_On_Boot();
 
+	// Ensure tw_sleep_reboot is reset in case 'adb reboot' was called
+	// instead of allowing Finish_TWRP() to clean-up previously.
+	DataManager::SetValue(TW_SLEEP_REBOOT_VAR, "0");
+
 	// Run any outstanding OpenRecoveryScript
 	if (DataManager::GetIntValue(TW_IS_ENCRYPTED) == 0 && (TWFunc::Path_Exists(SCRIPT_FILE_TMP) || TWFunc::Path_Exists(SCRIPT_FILE_CACHE))) {
 		OpenRecoveryScript::Run_OpenRecoveryScript();
