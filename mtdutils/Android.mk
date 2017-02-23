4f9fca8c1268303a5e16ce20adf00644b73280e9
Matt Mower <mowerm@gmail.com>
2017-02-22 19:51:23 -0600

    Cleanup mtdutils makefile
    Change-Id: If341ff14c04f8a35f61b98e8701db67f0345736a

--- mtdutils/Android.mk
+++ mtdutils/Android.mk
@@ -1,61 +1,78 @@
-ifneq ($(TARGET_SIMULATOR),true)
-
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	mtdutils.c \
-	mounts.c 
+    mounts.c \
+    mtdutils.c
 
 ifneq ($(filter rk30xx rk3188,$(TARGET_BOARD_PLATFORM)),)
-LOCAL_SRC_FILES += rk3xhack.c
-LOCAL_CFLAGS += -DRK3X
+    LOCAL_SRC_FILES += rk3xhack.c
+    LOCAL_CFLAGS += -DRK3X
 endif
 
-LOCAL_MODULE := libmtdutils
-LOCAL_STATIC_LIBRARIES := libcutils libc
+LOCAL_STATIC_LIBRARIES := \
+    libc \
+    libcutils
+
 LOCAL_CLANG := true
+
+LOCAL_MODULE := libmtdutils
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	mtdutils.c \
-	mounts.c 
+    mounts.c \
+    mtdutils.c
 
 ifneq ($(filter rk30xx rk3188,$(TARGET_BOARD_PLATFORM)),)
-LOCAL_SRC_FILES += rk3xhack.c
-LOCAL_CFLAGS += -DRK3X
+    LOCAL_SRC_FILES += rk3xhack.c
+    LOCAL_CFLAGS += -DRK3X
 endif
 
-LOCAL_MODULE := libmtdutils
-LOCAL_SHARED_LIBRARIES := libcutils libc
+LOCAL_SHARED_LIBRARIES := \
+    libc \
+    libcutils
+
 LOCAL_CLANG := true
+
+LOCAL_MODULE := libmtdutils
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
 ifeq ($(BOARD_USES_BML_OVER_MTD),true)
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := bml_over_mtd.c
-LOCAL_C_INCLUDES += $(commands_recovery_local_path)/mtdutils
-LOCAL_MODULE := libbml_over_mtd
-LOCAL_MODULE_TAGS := eng
-LOCAL_CFLAGS += -Dmain=bml_over_mtd_main
-include $(BUILD_STATIC_LIBRARY)
+    include $(CLEAR_VARS)
 
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := bml_over_mtd.c
-LOCAL_MODULE := bml_over_mtd
-LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
-LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
-LOCAL_MODULE_STEM := bml_over_mtd
-LOCAL_C_INCLUDES += $(commands_recovery_local_path)/mtdutils
-LOCAL_SHARED_LIBRARIES := libmtdutils libcutils liblog libc
-include $(BUILD_EXECUTABLE)
+    LOCAL_SRC_FILES := bml_over_mtd.c
+
+    LOCAL_CFLAGS := -Dmain=bml_over_mtd_main
+
+    LOCAL_MODULE := libbml_over_mtd
+    LOCAL_MODULE_TAGS := optional
+
+    include $(BUILD_STATIC_LIBRARY)
+
+    include $(CLEAR_VARS)
+
+    LOCAL_SRC_FILES := bml_over_mtd.c
+
+    LOCAL_SHARED_LIBRARIES := \
+        libc \
+        libcutils \
+        liblog \
+        libmtdutils
+
+    LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
+
+    LOCAL_MODULE := bml_over_mtd
+    LOCAL_MODULE_STEM := bml_over_mtd
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
+    LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+
+    include $(BUILD_EXECUTABLE)
 endif
-
-endif	# !TARGET_SIMULATOR
-
