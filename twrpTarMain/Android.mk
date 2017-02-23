dcdc1f42398b14db172b129730ccfe7731c8bda2
Matt Mower <mowerm@gmail.com>
2017-02-22 19:34:22 -0600

    Cleanup twrpTarMain makefile
    Change-Id: Ief7f25d2308a1bed96de36ec46b9188f2261bf35

--- twrpTarMain/Android.mk
+++ twrpTarMain/Android.mk
@@ -1,82 +1,109 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
-# Build static binary
+# Static executable
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= \
-	twrpTarMain.cpp \
-	../twrp-functions.cpp \
-	../twrpTar.cpp \
-	../tarWrite.c \
-	../exclude.cpp \
-	../progresstracking.cpp \
-	../gui/twmsg.cpp
-LOCAL_CFLAGS:= -g -c -W -DBUILD_TWRPTAR_MAIN
+LOCAL_SRC_FILES := \
+    ../exclude.cpp \
+    ../gui/twmsg.cpp \
+    ../progresstracking.cpp \
+    ../tarWrite.c \
+    ../twrp-functions.cpp \
+    ../twrpTar.cpp \
+    twrpTarMain.cpp
 
-LOCAL_C_INCLUDES += bionic
+LOCAL_CFLAGS := \
+    -DBUILD_TWRPTAR_MAIN \
+    -g -c -W
 
-LOCAL_STATIC_LIBRARIES := libc libtar_static libz
+ifneq ($(RECOVERY_SDCARD_ON_DATA),)
+    LOCAL_CFLAGS += -DRECOVERY_SDCARD_ON_DATA
+endif
+ifeq ($(TW_EXCLUDE_ENCRYPTED_BACKUPS), true)
+    LOCAL_CFLAGS += -DTW_EXCLUDE_ENCRYPTED_BACKUPS
+endif
+
+LOCAL_STATIC_LIBRARIES := \
+    libc \
+    libselinux \
+    libstdc++ \
+    libtar_static \
+    libz
+
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
-    LOCAL_C_INCLUDES += external/stlport/stlport bionic/libstdc++/include
     LOCAL_STATIC_LIBRARIES += libstlport_static
 endif
-LOCAL_STATIC_LIBRARIES += libstdc++
-
-LOCAL_C_INCLUDES += external/libselinux/include
-LOCAL_STATIC_LIBRARIES += libselinux
-
-ifneq ($(RECOVERY_SDCARD_ON_DATA),)
-	LOCAL_CFLAGS += -DRECOVERY_SDCARD_ON_DATA
-endif
-ifeq ($(TW_EXCLUDE_ENCRYPTED_BACKUPS), true)
-    LOCAL_CFLAGS += -DTW_EXCLUDE_ENCRYPTED_BACKUPS
-else
-	LOCAL_STATIC_LIBRARIES += libopenaes_static
+ifneq ($(TW_EXCLUDE_ENCRYPTED_BACKUPS), true)
+    LOCAL_STATIC_LIBRARIES += libopenaes_static
 endif
 
-LOCAL_MODULE:= twrpTar_static
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_MODULE_TAGS:= optional
-LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
-include $(BUILD_EXECUTABLE)
+LOCAL_C_INCLUDES := \
+    bionic \
+    external/libselinux/include
 
-
-# Build shared binary
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-	twrpTarMain.cpp \
-	../twrp-functions.cpp \
-	../twrpTar.cpp \
-	../tarWrite.c \
-	../exclude.cpp \
-	../progresstracking.cpp \
-	../gui/twmsg.cpp
-LOCAL_CFLAGS:= -g -c -W -DBUILD_TWRPTAR_MAIN
-
-LOCAL_C_INCLUDES += bionic
-LOCAL_SHARED_LIBRARIES := libc libtar libz
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
     LOCAL_C_INCLUDES += external/stlport/stlport bionic/libstdc++/include
-    LOCAL_SHARED_LIBRARIES += libstlport_static
 endif
-LOCAL_SHARED_LIBRARIES += libstdc++
 
-LOCAL_C_INCLUDES += external/libselinux/include
-LOCAL_SHARED_LIBRARIES += libselinux
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+LOCAL_MODULE := twrpTar_static
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+
+include $(BUILD_EXECUTABLE)
+
+# Shared executable
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    ../exclude.cpp \
+    ../gui/twmsg.cpp \
+    ../progresstracking.cpp \
+    ../tarWrite.c \
+    ../twrp-functions.cpp \
+    ../twrpTar.cpp \
+    twrpTarMain.cpp
+
+LOCAL_CFLAGS := \
+    -DBUILD_TWRPTAR_MAIN \
+    -g -c -W
 
 ifneq ($(RECOVERY_SDCARD_ON_DATA),)
-	LOCAL_CFLAGS += -DRECOVERY_SDCARD_ON_DATA
+    LOCAL_CFLAGS += -DRECOVERY_SDCARD_ON_DATA
 endif
 ifeq ($(TW_EXCLUDE_ENCRYPTED_BACKUPS), true)
     LOCAL_CFLAGS += -DTW_EXCLUDE_ENCRYPTED_BACKUPS
-else
-	LOCAL_SHARED_LIBRARIES += libopenaes
 endif
 
-LOCAL_MODULE:= twrpTar
-LOCAL_MODULE_TAGS:= optional
+LOCAL_C_INCLUDES := \
+    bionic \
+    external/libselinux/include
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
+    LOCAL_C_INCLUDES += \
+        bionic/libstdc++/include \
+        external/stlport/stlport
+endif
+
+LOCAL_SHARED_LIBRARIES := \
+    libc \
+    libselinux \
+    libstdc++ \
+    libtar \
+    libz
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
+    LOCAL_SHARED_LIBRARIES += libstlport_static
+endif
+ifneq ($(TW_EXCLUDE_ENCRYPTED_BACKUPS), true)
+    LOCAL_SHARED_LIBRARIES += libopenaes
+endif
+
+LOCAL_MODULE := twrpTar
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+
 include $(BUILD_EXECUTABLE)
