87c3bcdfccbcc1bc704e8cd25cd728196b8b5a3f
Matt Mower <mowerm@gmail.com>
2017-02-22 19:40:29 -0600

    Cleanup gpt makefile
    Change-Id: Ia11681cdeb9b31e8ae50d9ddb20a3aa7a45e26d2

--- gpt/Android.mk
+++ gpt/Android.mk
@@ -1,15 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-# Build libgpt_twrp library
-
 include $(CLEAR_VARS)
-LOCAL_CLANG := false
-LOCAL_MODULE := libgpt_twrp
-LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES = \
+LOCAL_SRC_FILES := \
     gpt.c \
     gptcrc32.c
 
 LOCAL_SHARED_LIBRARIES := libc
+
+LOCAL_CLANG := false
+LOCAL_MODULE := libgpt_twrp
+LOCAL_MODULE_TAGS := optional
+
 include $(BUILD_SHARED_LIBRARY)
