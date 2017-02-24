ebaadeffd5f23f26484167738f34e35176b4473a
dianlujitao <dianlujitao@lineageos.org>
2017-02-24 15:32:57 +0800

    Fix for LineageOS build
     * Match LineageOS cryptfs_hw location
     * Fallback to toybox if busybox is not persent, and warn about this
    
    Change-Id: I5bc1399602f59f4d22827ff7e47acb901b1e33c6

--- Android.mk
+++ Android.mk
@@ -178,6 +178,19 @@ ifeq ($(AB_OTA_UPDATER),true)
     LOCAL_ADDITIONAL_DEPENDENCIES += libhardware
 endif
 
+ifeq ($(wildcard external/busybox/Android.mk),)
+    TW_USE_TOOLBOX := true
+    $(warning **********************************)
+    $(warning * You're using toybox for TWRP,  *)
+    $(warning * some tools are not available,  *)
+    $(warning * busybox is highly recommanded! *)
+    $(warning **********************************)
+endif
+
+ifeq ($(shell test $(CM_PLATFORM_SDK_VERSION) -ge 7; echo $$?),0)
+    TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/cryptfs_hw
+endif
+
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 
 #ifeq ($(TARGET_RECOVERY_UI_LIB),)
