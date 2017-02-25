c8f8a56e476920f4965bdaa3cd18a177dd7c2d44
Matt Mower <mowerm@gmail.com>
2017-02-25 10:15:54 -0600

    Do not nest sleep action inside sleepcounter action
    Change-Id: I14a2bd57033665bf6558792204cb56ec5bee6f70

--- gui/pages.cpp
+++ gui/pages.cpp
@@ -55,6 +55,7 @@ extern "C" {
  * 2 - Theme handles power button as action togglebacklight
  * 3 - Create generalized sleep_and_reboot page
  *     Replace several main2 page jumps with main
+ *     Do not nest sleep action inside sleepcounter action
  */
 #define TW_THEME_VERSION 3
 
