33054e22f317c635fd1f528d5df85d0f67262d37
bigbiff bigbiff <bigbiff@teamw.in>
2017-03-20 18:53:53 -0400

    Set Backup_FileName on adb backup.
    Change-Id: I7eb8168fa3416366b996e220cecbc0e1bcec5e8e

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -915,6 +915,11 @@ int TWPartitionManager::Run_Backup(bool adbbackup) {
 bool TWPartitionManager::Restore_Partition(PartitionSettings *part_settings) {
 	time_t Start, Stop;
 
+	if (part_settings->adbbackup) {
+		part_settings->Part->Backup_FileName = part_settings->Part->Backup_Name + "." + part_settings->Part->Current_File_System + ".win";;
+	}
+;
+
 	TWFunc::SetPerformanceMode(true);
 
 	time(&Start);
