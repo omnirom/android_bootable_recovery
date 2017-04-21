ac31808cd37cfb98755e5821dbb2efb5fe5cb12a
Jin Qian <jinqian@google.com>
2017-04-21 14:36:12 -0700

    recovery: replace make_ext4 with e2fsprogs
    Execute mke2fs to create empty ext4 filesystem.
    Execute e2fsdroid to add files to filesystem.
    
    Test: enter recovery mode and wipe data
    Bug: 35219933
    Change-Id: I10a9f4c1f4754ad864b2df45b1f879180ab33876

--- Android.mk
+++ Android.mk
@@ -84,9 +84,11 @@ LOCAL_MODULE := recovery
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
+LOCAL_REQUIRED_MODULES := e2fsdroid_static mke2fs_static mke2fs.conf
+
 ifeq ($(TARGET_USERIMAGES_USE_F2FS),true)
 ifeq ($(HOST_OS),linux)
-LOCAL_REQUIRED_MODULES := mkfs.f2fs
+LOCAL_REQUIRED_MODULES += mkfs.f2fs
 endif
 endif
 
@@ -101,6 +103,7 @@ LOCAL_STATIC_LIBRARIES := \
     libverifier \
     libbatterymonitor \
     libbootloader_message \
+    libfs_mgr \
     libext4_utils \
     libsparse \
     libziparchive \
@@ -111,7 +114,6 @@ LOCAL_STATIC_LIBRARIES := \
     libfusesideload \
     libminui \
     libpng \
-    libfs_mgr \
     libcrypto_utils \
     libcrypto \
     libvintf_recovery \
@@ -140,7 +142,7 @@ else
 endif
 
 ifeq ($(BOARD_CACHEIMAGE_PARTITION_SIZE),)
-LOCAL_REQUIRED_MODULES := recovery-persist recovery-refresh
+LOCAL_REQUIRED_MODULES += recovery-persist recovery-refresh
 endif
 
 include $(BUILD_EXECUTABLE)
