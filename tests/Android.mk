24bab361b7ead7fd2c0be1cb08bb0d63741165cd
Adrian DC <radian.dc@gmail.com>
2017-10-25 23:22:07 +0200

    Revert "kill package_extract_dir"
     * Also add missing dependency to libziparchive
    
    This reverts commit 53c38b15381ace565227e49104a6fd64c4c28dcc.
    Change-Id: I050c790c58bab104278b9201e2dc52679584fcf4

--- tests/Android.mk
+++ tests/Android.mk
@@ -45,6 +45,7 @@ LOCAL_SRC_FILES := \
     unit/rangeset_test.cpp \
     unit/sysutil_test.cpp \
     unit/zip_test.cpp \
+    unit/ziputil_test.cpp
 
 LOCAL_C_INCLUDES := $(RECOVERY_PATH)
 LOCAL_SHARED_LIBRARIES := liblog
