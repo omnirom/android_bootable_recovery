a662070332e9b77aa9ce05379ec2d320f16a07d1
Michael Bestas <mkbestas@lineageos.org>
2018-01-22 03:21:47 +0200

    Revert "Fix issues building with LineageOS cryptfs_hw"
    * LineageOS 15.1+ has cryptfs_hw in vendor as well
    
    This reverts commit dcd9f3f384f9497916118173ece71cd258cab876.
    
    Change-Id: If730b44d9aa7d6992f6933812254f45fc83413ef

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -173,11 +173,7 @@ ifeq ($(TW_INCLUDE_CRYPTO), true)
     RELINK_SOURCE_FILES += $(TARGET_OUT_SHARED_LIBRARIES)/libhardware.so
     RELINK_SOURCE_FILES += $(TARGET_OUT_SHARED_LIBRARIES)/libgpt_twrp.so
     ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-        ifeq ($(TARGET_CRYPTFS_HW_PATH),)
-            RELINK_SOURCE_FILES += $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libcryptfs_hw.so
-        else
-            RELINK_SOURCE_FILES += $(TARGET_OUT_SHARED_LIBRARIES)/libcryptfs_hw.so
-        endif
+        RELINK_SOURCE_FILES += $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libcryptfs_hw.so
     endif
     # FBE files
     ifeq ($(TW_INCLUDE_CRYPTO_FBE), true)
