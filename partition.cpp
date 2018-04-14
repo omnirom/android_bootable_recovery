5199e42b2289f85af1a1106f0b113b8183a112c7
Ícaro Hoff <icarohoff@gmail.com>
2018-04-14 09:19:22 -0300

    partition: properly adjust mkfs.f2fs lenght reservation
    The mkfs.f2fs binary doesn't have a block reservation argument, the
    workaround is to calculate the total number of blocks and subtract the
    length (for crypto footer) from it.
    
    Converted from https://github.com/LineageOS/android_system_core/commit/9d1ed1a7ff9dd10e8d697b41d3b1efbd709de7ba to TWRP.
    
    Test: Cryptfs is capable of setting up F2FS encryption on dm-0.
    
    Change-Id: I31be9e562e3c59b2f6f662d03da3aaf4348f2be3
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- partition.cpp
+++ partition.cpp
@@ -2217,17 +2217,29 @@ bool TWPartition::Wipe_F2FS() {
 		gui_msg(Msg("formatting_using=Formatting {1} using {2}...")(Display_Name)("mkfs.f2fs"));
 		Find_Actual_Block_Device();
 		command = "mkfs.f2fs -t 0";
+		command += " " + Actual_Block_Device;
 		if (!Is_Decrypted && Length != 0) {
 			// Only use length if we're not decrypted
-			char len[32];
-			int mod_length = Length;
-			if (Length < 0)
-				mod_length *= -1;
-			sprintf(len, "%i", mod_length);
-			command += " -r ";
-			command += len;
+			unsigned long long Actual_Size = IOCTL_Get_Block_Size();
+			if (Actual_Size == 0)
+				return false;
+
+			unsigned long long Length_Pos;
+			if (Length < 0) {
+				// Convert to positive length
+				Length_Pos = (Length * -1);
+				// Subtract it from the total amount of blocks
+				Actual_Size -= Length_Pos;
+			}
+			// Divide the actual size by the sector-size
+			unsigned long long Block_Count;
+			Block_Count = (Actual_Size / 4096LLU);
+			char temp[256];
+			sprintf(temp, "%llu", Block_Count);
+			command += " ";
+			command += temp;
 		}
-		command += " " + Actual_Block_Device;
+		LOGINFO("mkfs.f2fs command: %s\n", command.c_str());
 		if (TWFunc::Exec_Cmd(command) == 0) {
 			Recreate_AndSec_Folder();
 			gui_msg("done=Done.");
