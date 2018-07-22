9d173f02513ea64669b80b22a117ee2f250ffcca
LuK1337 <priv.luk@gmail.com>
2018-07-22 17:22:09 +0200

    TWRP: Fix system image generation for AB targets
    On AB targets, file_contexts.bin will be copied to system
    and therefore will fail during creation of the system image
    because of it being unlabeled.
    
    Change-Id: I93800f0cc388437f2f1bee268dd1e54a32269ed4

--- Android.mk
+++ Android.mk
@@ -494,7 +494,8 @@ LOCAL_MODULE := file_contexts_text
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := file_contexts.bin
 LOCAL_POST_INSTALL_CMD := \
-    $(hide) cp -f $(OUT)/obj/ETC/file_contexts.bin_intermediates/file_contexts.concat.tmp $(TARGET_RECOVERY_ROOT_OUT)/file_contexts
+    $(hide) cp -f $(OUT)/obj/ETC/file_contexts.bin_intermediates/file_contexts.concat.tmp $(TARGET_RECOVERY_ROOT_OUT)/file_contexts && \
+    rm $(TARGET_ROOT_OUT)/file_contexts.bin
 
 include $(BUILD_PHONY_PACKAGE)
 
