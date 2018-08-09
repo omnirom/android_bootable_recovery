d2be7fc29a0aed6275cbe7cd26a0840c7ebbb675
Tao Bao <tbao@google.com>
2018-08-09 01:24:33 -0700

    updater_sample: Move to Soong.
    Test: Follow the steps in "Running tests" in updater_sample/README.md.
    Change-Id: I70c3239e3daea0dcdadf02f68abe7d6aafa67a2e

--- Android.mk
+++ Android.mk
@@ -246,4 +246,3 @@ include \
     $(LOCAL_PATH)/boot_control/Android.mk \
     $(LOCAL_PATH)/tests/Android.mk \
     $(LOCAL_PATH)/updater/Android.mk \
-    $(LOCAL_PATH)/updater_sample/Android.mk \
