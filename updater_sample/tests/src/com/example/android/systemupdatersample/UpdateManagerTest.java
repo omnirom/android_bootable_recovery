d5c7fb5e6efb98a4b8072be3c1e7e505b3344466
koushik panuganti <kpanuganti@google.com>
2018-12-12 10:39:44 -0800

    Migrate bootable/recovery to androidx.test
    See go/jetpack-test-android-migration
    
    Test: make checkbuild
    Change-Id: I0740a2205e6b3893ba292cd841592ea85071eefb

--- updater_sample/tests/src/com/example/android/systemupdatersample/UpdateManagerTest.java
+++ updater_sample/tests/src/com/example/android/systemupdatersample/UpdateManagerTest.java
@@ -26,9 +26,10 @@ import static org.mockito.Mockito.when;
 import android.content.Context;
 import android.os.UpdateEngine;
 import android.os.UpdateEngineCallback;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.filters.SmallTest;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
 
 import com.example.android.systemupdatersample.tests.R;
 import com.example.android.systemupdatersample.util.PayloadSpecs;
--- updater_sample/tests/src/com/example/android/systemupdatersample/util/FileDownloaderTest.java
+++ updater_sample/tests/src/com/example/android/systemupdatersample/util/FileDownloaderTest.java
@@ -19,9 +19,10 @@ package com.example.android.systemupdatersample.util;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.filters.SmallTest;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
 
 import com.example.android.systemupdatersample.tests.R;
 
