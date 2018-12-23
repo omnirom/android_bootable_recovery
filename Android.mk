bf65aec13d4e360939ec298f3a16cfb9c125a617
oshmoun <oshmoun100@googlemail.com>
2018-12-23 18:35:41 +0100

    Android.mk: do not include file_contexts for P and up
    Change-Id: Idf491e83f45821b15af88ca502852f10752e006a
    Signed-off-by: nailyk <jenkins@nailyk.fr>

--- Android.mk
+++ Android.mk
@@ -515,10 +515,13 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 28; echo $$?),0)
 endif
 endif
 
+# Only add file_contexts for Android versions N and O
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -le 27; echo $$?),0)
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26; echo $$?),0)
     LOCAL_REQUIRED_MODULES += file_contexts_text
 else ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 25; echo $$?),0)
     LOCAL_ADDITIONAL_DEPENDENCIES += file_contexts_text
+endif
 endif
 
 ifeq ($(BOARD_CACHEIMAGE_PARTITION_SIZE),)
