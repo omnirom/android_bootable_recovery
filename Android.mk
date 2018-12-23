eb0d1f846e8bc55fd582565ec944a3fe01b08a2b
oshmoun <oshmoun100@googlemail.com>
2018-12-23 18:35:41 +0100

    Android.mk: do not include file_contexts for P and up
    Change-Id: Idf491e83f45821b15af88ca502852f10752e006a
    Signed-off-by: nailyk <jenkins@nailyk.fr>

--- Android.mk
+++ Android.mk
@@ -515,8 +515,10 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 28; echo $$?),0)
 endif
 endif
 
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 25; echo $$?),0)
-    TWRP_REQUIRED_MODULES += file_contexts_text
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -le 27; echo $$?),0)
+    ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 25; echo $$?),0)
+        TWRP_REQUIRED_MODULES += file_contexts_text
+    endif
 endif
 
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 24; echo $$?),0)
