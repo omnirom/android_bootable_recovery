f6a247d0c9f1c0c3d98dac48669581ed8eff4f5d
bigbiff bigbiff <bigbiff@teamw.in>
2019-01-08 20:06:57 -0500

    ORS: Use built in check for AB devices to determine where cache is stored on the device.
    Change-Id: I485ec205a604f1f07ceb7700ae9c4e720fd55c3e

--- openrecoveryscript.cpp
+++ openrecoveryscript.cpp
@@ -62,17 +62,20 @@ OpenRecoveryScript::VoidFunction OpenRecoveryScript::call_after_cli_command;
 #define SCRIPT_COMMAND_SIZE 512
 
 int OpenRecoveryScript::check_for_script_file(void) {
-	if (!PartitionManager.Mount_By_Path(SCRIPT_FILE_CACHE, false)) {
-		LOGINFO("Unable to mount /cache for OpenRecoveryScript support.\n");
-		gui_msg(Msg(msg::kError, "unable_to_mount=Unable to mount {1}")(SCRIPT_FILE_CACHE));
-		return 0;
+	std::string orsFile = TWFunc::get_cache_dir() + "/openrecoveryscript";
+	if (TWFunc::get_cache_dir() == "/cache/") {
+		if (!PartitionManager.Mount_By_Path(TWFunc::get_cache_dir(), false)) {
+			LOGINFO("Unable to mount %s for OpenRecoveryScript support.\n", TWFunc::get_cache_dir().c_str());
+			gui_msg(Msg(msg::kError, "unable_to_mount=Unable to mount {1}")(TWFunc::get_cache_dir().c_str()));
+			return 0;
+		}
 	}
-	if (TWFunc::Path_Exists(SCRIPT_FILE_CACHE)) {
-		LOGINFO("Script file found: '%s'\n", SCRIPT_FILE_CACHE);
+	if (TWFunc::Path_Exists(orsFile)) {
+		LOGINFO("Script file found: '%s'\n", orsFile.c_str());
 		// Copy script file to /tmp
-		TWFunc::copy_file(SCRIPT_FILE_CACHE, SCRIPT_FILE_TMP, 0755);
+		TWFunc::copy_file(orsFile, SCRIPT_FILE_TMP, 0755);
 		// Delete the file from /cache
-		unlink(SCRIPT_FILE_CACHE);
+		unlink(orsFile.c_str());
 		return 1;
 	}
 	return 0;
@@ -655,6 +658,10 @@ void OpenRecoveryScript::Run_CLI_Command(const char* command) {
 		gui_msg("decrypt_cmd=Attempting to decrypt data partition via command line.");
 		if (PartitionManager.Decrypt_Device(pass) == 0) {
 			// set_page_done = 1;  // done by singleaction_page anyway
+			std::string orsFile = TWFunc::get_cache_dir() + "/openrecoveryscript";
+			if (TWFunc::Path_Exists(orsFile)) {
+				Run_OpenRecoveryScript_Action();
+			}
 		}
 	} else if (OpenRecoveryScript::Insert_ORS_Command(command)) {
 		OpenRecoveryScript::run_script_file();
