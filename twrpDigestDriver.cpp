c53d520d2b6e04db0c735f1b4c43e4ea27efa309
Ethan Yonker <dees_troy@teamw.in>
2019-04-02 15:56:25 -0500

    Fix zip digest check
    Change-Id: I5c05c485574131d3fbc8cb5af5b1281d12b822ce

--- twrpDigestDriver.cpp
+++ twrpDigestDriver.cpp
@@ -69,10 +69,12 @@ bool twrpDigestDriver::Check_File_Digest(const string& Filename) {
 #endif
 
 	if (!TWFunc::Path_Exists(digestfile)) {
+		delete digest;
 		if (Filename.find(".zip") == std::string::npos) {
 			gui_msg(Msg(msg::kError, "no_digest_found=No digest file found for '{1}'. Please unselect Enable Digest verification to restore.")(Filename));
+		} else {
+			return true;
 		}
-		delete digest;
 		return false;
 	}
 
