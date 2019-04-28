d0a188464182c04a81e0d85c5c20d59e1bf400cc
Noah Jacobson <noahajac@gmail.com>
2019-04-28 00:10:07 -0400

    Add support for multi-user decryption
    * Add CLI support using the command "twrp decrypt <PASSWORD> [USER ID]"
    * Add GUI support using the menu Advanced --> Decrypt User
    
    Change-Id: Id2681bc2b1012a0571417f7a6e0b980b1b25c61c

--- gui/action.cpp
+++ gui/action.cpp
@@ -1494,26 +1494,37 @@ int GUIAction::decrypt(std::string arg __unused)
 		simulate_progress_bar();
 	} else {
 		string Password;
+		string userID;
 		DataManager::GetValue("tw_crypto_password", Password);
-		op_status = PartitionManager.Decrypt_Device(Password);
-		if (op_status != 0)
-			op_status = 1;
-		else {
+		DataManager::GetValue("tw_crypto_userid", userID);
 
-			DataManager::SetValue(TW_IS_ENCRYPTED, 0);
+		if (userID != "") {
+			op_status = PartitionManager.Decrypt_Device(Password, stoi(userID));
+			if (op_status != 0)
+				op_status = 1;
+			operation_end(op_status);
+			return 0;
+		} else {
+			op_status = PartitionManager.Decrypt_Device(Password);
+			if (op_status != 0)
+				op_status = 1;
+			else {
 
-			int has_datamedia;
+				DataManager::SetValue(TW_IS_ENCRYPTED, 0);
 
-			// Check for a custom theme and load it if exists
-			DataManager::GetValue(TW_HAS_DATA_MEDIA, has_datamedia);
-			if (has_datamedia != 0) {
-				if (tw_get_default_metadata(DataManager::GetSettingsStoragePath().c_str()) != 0) {
-					LOGINFO("Failed to get default contexts and file mode for storage files.\n");
-				} else {
-					LOGINFO("Got default contexts and file mode for storage files.\n");
+				int has_datamedia;
+
+				// Check for a custom theme and load it if exists
+				DataManager::GetValue(TW_HAS_DATA_MEDIA, has_datamedia);
+				if (has_datamedia != 0) {
+					if (tw_get_default_metadata(DataManager::GetSettingsStoragePath().c_str()) != 0) {
+						LOGINFO("Failed to get default contexts and file mode for storage files.\n");
+					} else {
+						LOGINFO("Got default contexts and file mode for storage files.\n");
+					}
 				}
+				PartitionManager.Decrypt_Adopted();
 			}
-			PartitionManager.Decrypt_Adopted();
 		}
 	}
 
