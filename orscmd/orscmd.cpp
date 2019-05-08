29969ec5dd406f36ff6a8cf1ae5acc8773f4e3d7
Nikolay <Mr.Kolibass@gmail.com>
2019-05-08 15:38:23 +0300

    Update help manual in orscmd
    Change-Id: I4b2e385637cded40dbb08e0ee8ca7117dbdd67ca

--- orscmd/orscmd.cpp
+++ orscmd/orscmd.cpp
@@ -47,6 +47,12 @@ void print_usage(void) {
 	printf("  set <variable> [value]\n");
 	printf("  decrypt <password>\n");
 	printf("  remountrw\n");
+	printf("  fixperms\n");
+	printf("  mount <path>\n");
+	printf("  unmount <path>\n");
+	printf("  print <value>\n");
+	printf("  mkdir <directory>\n");
+	printf("  reboot [recovery|poweroff|bootloader|download|edl]\n");
 	printf("\nSee more documentation at https://twrp.me/faq/openrecoveryscript.html\n");
 }
 
