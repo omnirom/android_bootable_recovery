f5955b1cb44c0ced5983207c55f0013a4586b284
bigbiff bigbiff <bigbiff@teamw.in>
2019-05-18 17:28:58 -0400

    Digest: Create digest for each subpartition and not just the first.
    Change-Id: I408382e83aa0ac1106ff7198adf9787c0a0877cd

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -690,6 +690,7 @@ bool TWPartitionManager::Backup_Partition(PartitionSettings *part_settings) {
 					}
 					sync();
 					sync();
+					string Full_Filename = part_settings->Backup_Folder + "/" + part_settings->Part->Backup_FileName;
 					if (!part_settings->adbbackup && part_settings->generate_digest) {
 						if (!twrpDigestDriver::Make_Digest(Full_Filename)) {
 							goto backup_error;
