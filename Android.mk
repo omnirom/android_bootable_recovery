744110e352120e5dda8648251ee871b80b2d1018
mauronofrio <poronofrio@gmail.com>
2019-10-26 19:47:55 +0200

    Add ozip decryption for Oppo/Realme device
    This function will automatically decrypt .ozip file in a .zip and it will
    save the .zip in the same folder of the .ozip and with the same name.
    After the decryption, the flash will start.
    
    To enable this function you have to set a Boardconfig flag with the
    decryption key like i did here:
    
    https://github.com/mauronofrio/android_device_realme_RMX1901/commit/a30625f80a31b9b85e4948fc9f9beeb61d5bcb8a
    
    
    Original projects:
    https://github.com/sabpprook/ozip2zip
    https://github.com/bkerler/oppo_ozip_decrypt
    
    Change-Id: Ic93243a565008c442bcf2992069b2801532ac7c1

--- Android.mk
+++ Android.mk
@@ -406,6 +406,7 @@ TWRP_REQUIRED_MODULES += \
     pigz \
     teamwin \
     toolbox_symlinks \
+    ozip_decrypt \
     twrp \
     fsck.fat \
     fatlabel \
@@ -860,6 +861,10 @@ include $(commands_TWRP_local_path)/injecttwrp/Android.mk \
 
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 24; echo $$?),0)
     include $(commands_TWRP_local_path)/libmincrypt/Android.mk
+endif
+
+ifneq ($(TW_OZIP_DECRYPT_KEY),)
+    include $(commands_TWRP_local_path)/ozip_decrypt/Android.mk
 endif
 
 ifeq ($(TW_INCLUDE_CRYPTO), true)
