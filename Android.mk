ca2ba939c989b19dc16ea80f46a560e0442cc1e0
Captain Throwback <captainthrowback@hotmail.com>
2020-10-14 12:45:52 -0400

    init: add services to build
    hwservicemanager and servicemanager are needed for decryption
    and fastbootd in TWRP. Include these services by default since
    they are standard Android services and independent of device
    tree or SoC.
    
    Change-Id: Id0ec73d9a6ca0289c7b4f67e9dcf541b2157c04e

--- Android.mk
+++ Android.mk
@@ -381,6 +381,8 @@ TWRP_REQUIRED_MODULES += \
     init.recovery.hlthchrg.rc \
     init.recovery.service.rc \
     init.recovery.ldconfig.rc \
+    hwservicemanager.rc \
+    servicemanager.rc \
     awk \
     toybox \
     toolbox \
