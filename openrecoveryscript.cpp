f27ba0e5484001a22898bab8b4576d65e1f3b877
Adrian DC <radian.dc@gmail.com>
2021-01-05 23:54:18 +0100

    ors: Refresh the system details after sideload and wipe system
     * Thus resolve the 'No OS installed' warnings upon system reboot
       if only adb shell twrp actions were used on the device
    
    Change-Id: Id4fd915ce25cef8a2fdef6fda1750ca1b3ed13b7
    (cherry picked from commit 0c6a6266fba8960f1b96a08c837ac61f2b7f9310)

--- openrecoveryscript.cpp
+++ openrecoveryscript.cpp
@@ -156,6 +156,7 @@ int OpenRecoveryScript::run_script_file(void) {
 					PartitionManager.Wipe_By_Path("/cache");
 				} else if (strcmp(value, "system") == 0 || strcmp(value, "/system") == 0 || strcmp(value, PartitionManager.Get_Android_Root_Path().c_str()) == 0) {
 					PartitionManager.Wipe_By_Path("/system");
+					PartitionManager.Update_System_Details();
 				} else if (strcmp(value, "dalvik") == 0 || strcmp(value, "dalvick") == 0 || strcmp(value, "dalvikcache") == 0 || strcmp(value, "dalvickcache") == 0) {
 					PartitionManager.Wipe_Dalvik_Cache();
 				} else if (strcmp(value, "data") == 0 || strcmp(value, "/data") == 0 || strcmp(value, "factory") == 0 || strcmp(value, "factoryreset") == 0) {
@@ -389,6 +390,7 @@ int OpenRecoveryScript::run_script_file(void) {
 					ret_val = 1; // failure
 				}
 				PartitionManager.Unlock_Block_Partitions();
+				PartitionManager.Update_System_Details();
 				sideload = 1; // Causes device to go to the home screen afterwards
 				pid_t sideload_child_pid = GetMiniAdbdPid();
 				if (sideload_child_pid != 0) {
