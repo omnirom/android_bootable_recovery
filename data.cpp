4bce54bcd7878dd8434c046eb740276cd4d8afd6
Captain Throwback <captainthrowback@hotmail.com>
2021-09-16 10:04:26 -0400

    Add exclusion criteria for "Fix Recovery Bootloop"
    Should only be included on devices that use initramfs,
    i.e. legacy SAR, recovery-in-boot
    
    Change-Id: I2408fcd993e1e381f49b3d5ae860694b2fade4d2

--- data.cpp
+++ data.cpp
@@ -781,6 +781,12 @@ void DataManager::SetDefaultValues()
 #endif
 	mPersist.SetValue(TW_UNMOUNT_SYSTEM, "1");
 
+#if defined BOARD_USES_RECOVERY_AS_BOOT && defined BOARD_BUILD_SYSTEM_ROOT_IMAGE
+	mConst.SetValue("tw_uses_initramfs", "1");
+#else
+	mConst.SetValue("tw_uses_initramfs", "0");
+#endif
+
 #ifdef TW_NO_SCREEN_TIMEOUT
 	mConst.SetValue("tw_screen_timeout_secs", "0");
 	mConst.SetValue("tw_no_screen_timeout", "1");
